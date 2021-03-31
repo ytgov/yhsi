ec0b75ec1833865855dbe6693367bb130150672f
Edgar Allan Glez <edgarallanglez@gmail.com>
2021-03-30 18:21:14 -0700

    Api structure for boats, in express with pm2, docker-compose, knex and jwt security

---
+++ src/api/controllers/owners.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+var authenticateToken = require('../utils');
+
+router.get('/', authenticateToken, async (req, res) => {
+  const permissions = req.decodedToken['yg-claims'].permissions;
+  if (!permissions.includes('view')) res.sendStatus(403);
+
+  const db = req.app.get('db');
+  const { page = 0, limit = 10 } = req.query;
+  const offset = (page*limit) || 0;
+
+  const owners = await db.select('boat.boatowner.currentowner', 'boat.Owner.OwnerName', 'boat.owner.id')
+    .distinct('boat.boatowner.ownerid')
+    .from('boat.boatowner')
+    .join('boat.Owner', 'boat.BoatOwner.ownerid', '=', 'boat.owner.id')
+    .orderBy('boat.boatowner.ownerid', 'asc')
+    .limit(limit).offset(offset);
+
+  res.status(200).send(owners);
+});
+
+router.get('/:ownerId', authenticateToken, async (req, res) => {
+  const permissions = req.decodedToken['yg-claims'].permissions;
+  if (!permissions.includes('view')) res.sendStatus(403);
+
+  const db = req.app.get('db');
+  const { ownerId } = req.params;
+
+  const owner = await db.select('*')
+    .distinct('boat.boatowner.ownerid')
+    .from('boat.boatowner')
+    .join('boat.Owner', 'boat.BoatOwner.ownerid', '=', 'boat.owner.id')
+    .where('boat.boatowner.ownerid', ownerId)
+    .first();
+  
+  owner.boats = await db.select('*')
+    .from('boat.boat')
+    .join('boat.BoatOwner', 'boat.BoatOwner.boatid', '=', 'boat.boat.id')
+    .where('boat.boatowner.ownerid', ownerId);
+
+  owner.histories = await db.select('*')
+    .from('boat.history')
+    .where('boat.history.uid', ownerId);
+  
+  owner.alias = await db.select('*')
+    .from('boat.owneralias')
+    .where('boat.owneralias.ownerid', ownerId);
+
+  res.status(200).send(owner);
+});
+
+
+module.exports = router
