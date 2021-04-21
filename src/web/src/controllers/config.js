fd5afe8996a38fba9a0862aa4d31d49e3b626fe2
Angel Gomez <angel@bizont.ca>
2021-04-21 13:12:59 -0600

    Front: added a config file on the controllers folder to handle TOKEN & BASE URL for all api calls

---
+++ src/web/src/controllers/config.js
@@ -0,0 +1,14 @@
+
+import axios from 'axios';
+
+const BASE_URL =  'http://localhost:8888/api/';
+const TEST_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ5Zy1jbGFpbXMiOnsicGVybWlzc2lvbnMiOlsidmlldyIsImNyZWF0ZSIsImVkaXQiXX0sInN1YiI6IjEyMzQ1Njc4OTAiLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9.BDa7etuFx-QoqTHxEwOpBaWdsbupbrL4YszziI_W7to'
+
+export default axios.create({
+    baseURL: BASE_URL,//this url is going to be replaced 
+    timeout: 24000,
+    headers : {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${TEST_TOKEN}`
+    }
+  });
