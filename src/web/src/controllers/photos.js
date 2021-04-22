3d589c7cb165d334a12caa26b547c5c47d2cbc32
Angel Gomez <angel@bizont.ca>
2021-04-22 12:05:19 -0600

    Front: Added a new endpoint connection to the photos component

--- src/web/src/controllers/photos.js
+++ src/web/src/controllers/photos.js
@@ -1,18 +1,7 @@
 import api from './config';
 
   export default {
-    async get(){
-        return await api.get(`photos`)
-        .then(res => {
-
-            console.log(res.data);
-          return res.data;
-        }).catch(error =>{
-          // handle error
-          console.log(error);
-        });
-    },
-    async getByPhotoId(id){//doesnt work rn
+    async getByBoatId(id){//doesnt work rn
       return await api.get(`photos/${id}`)
       .then(res => {
         return res.data;
