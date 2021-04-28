e2aec8f0ee5bf1936e98e904c8120525a5b0f23c
Angel Gomez <angel@bizont.ca>
2021-04-28 12:15:59 -0600

    Front: Added a new component to locate crash sites using google maps

---
+++ src/web/src/components/AirplaneCrashes/AirplaneComponents/GoogleMapLoader.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="map" style="height: 400px;" ref="googleMap"></div>
+</template>
+
+<script>
+import { Loader } from '@googlemaps/js-api-loader';
+export default {
+    data: () =>({
+        loader: null,
+        apiKey:  "AIzaSyB-Wc3ljVbi-897ttA4gUi216Da6um59fk",
+        mapConfig: {},
+        map: null
+    }),
+    async mounted() {
+        this.loader = new Loader({
+            apiKey: this.apiKey,
+            version: "weekly",
+            libraries: ["places"]
+            });
+        this.loader.load().then(() => {
+            this.map = new window.google.maps.Map(this.$refs.googleMap, {
+                center: { lat: -34.397, lng: 150.644 },
+                zoom: 8,
+            });
+        });
+
+    }, 
+    methods:{
+
+    }
+}
+</script>
