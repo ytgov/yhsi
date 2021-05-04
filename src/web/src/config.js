1e9e534059d7f43878cbd8e93e62800f997d2abf
Edgar Allan Glez <edgarallanglez@gmail.com>
2021-05-04 11:14:59 -0700

    frontend refactor to build on docker

--- src/web/src/config.js
+++ src/web/src/config.js
@@ -61,4 +61,4 @@ export const sections = [
     // }
 ];
 export const environment = process.env.NODE_ENV;
-export const apiBaseUrl = process.env.NODE_ENV == "production" ? "" : "http://localhost:3000";
+export const apiBaseUrl = process.env.NODE_ENV == "production" ? "" : "http://localhost:4125";
