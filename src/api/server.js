0195373e0cacd7558420a9f4956d0ecb25c3b6ff
Edgar Allan Glez <edgarallanglez@gmail.com>
2021-05-14 12:15:59 -0600

    open cors to test with outside CDN

--- src/api/server.js
+++ src/api/server.js
@@ -19,6 +19,7 @@ var _ = require('lodash');
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: false })) // for parsing application/x-www-form-urlencoded
 app.use(cors()); //only for dev ** remove if its not needed for prod
+app.options('*', cors())
 
 var conn = knex({
   client: 'mssql',
