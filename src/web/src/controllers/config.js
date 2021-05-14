c627176900e50e4ef5a20ecc22a66cbf1102aeac
Edgar Allan Glez <edgarallanglez@gmail.com>
2021-05-14 13:50:11 -0600

    enable cors

--- src/web/src/controllers/config.js
+++ src/web/src/controllers/config.js
@@ -5,15 +5,13 @@ const BASE_URL =  'https://api.gov.yk.ca/heritage/api/';
 // const BASE_URL =  'http://localhost:4125/api/';
 const TEST_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ5Zy1jbGFpbXMiOnsicGVybWlzc2lvbnMiOlsidmlldyIsImNyZWF0ZSIsImVkaXQiXX0sInN1YiI6IjEyMzQ1Njc4OTAiLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9.BDa7etuFx-QoqTHxEwOpBaWdsbupbrL4YszziI_W7to'
 
+axios.defaults.headers.common['Authorization'] = `Bearer ${TEST_TOKEN}`;
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.headers.post['Ocp-Apim-Subscription-Key'] = '5d23d587d6674b88b29349f12da7a406';
+
 const api =  axios.create({
   baseURL: BASE_URL,//this url is going to be replaced 
   timeout: 24000,
-  headers : {
-    // 'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${TEST_TOKEN}`,
-    'Ocp-Apim-Subscription-Key': '5d23d587d6674b88b29349f12da7a406'
-  }
 });
 
 const apiP  =  axios.create({
@@ -22,8 +20,6 @@ const apiP  =  axios.create({
   headers : {
     // 'Access-Control-Allow-Origin': '*',
     'Content-Type': 'multipart/form-data',
-    'Authorization': `Bearer ${TEST_TOKEN}`,
-    'Ocp-Apim-Subscription-Key': '5d23d587d6674b88b29349f12da7a406'
   }
 });
 
