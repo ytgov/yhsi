acfd749465ea5a7b8d2f30f71db11c97f9661411
Edgar Allan Glez <edgarallanglez@gmail.com>
2021-05-14 12:44:01 -0600

    open cors to test with outside CDN

--- src/web/src/controllers/config.js
+++ src/web/src/controllers/config.js
@@ -9,7 +9,7 @@ const api =  axios.create({
   baseURL: BASE_URL,//this url is going to be replaced 
   timeout: 24000,
   headers : {
-    'Access-Control-Allow-Origin': '*',
+    // 'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
     'Authorization': `Bearer ${TEST_TOKEN}`,
     'Ocp-Apim-Subscription-Key': '5d23d587d6674b88b29349f12da7a406'
@@ -20,7 +20,7 @@ const apiP  =  axios.create({
   baseURL: BASE_URL,//this url is going to be replaced 
   timeout: 24000,
   headers : {
-    'Access-Control-Allow-Origin': '*',
+    // 'Access-Control-Allow-Origin': '*',
     'Content-Type': 'multipart/form-data',
     'Authorization': `Bearer ${TEST_TOKEN}`,
     'Ocp-Apim-Subscription-Key': '5d23d587d6674b88b29349f12da7a406'
