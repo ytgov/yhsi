9599a2962827237b5e4e7bb062d236b5921edbfb
Angel Gomez <angel@bizont.ca>
2021-05-26 12:52:56 -0600

    Front: finished the aircrash pdf

--- src/web/src/components/AirplaneCrashes/AirplaneComponents/PrintButton.vue
+++ src/web/src/components/AirplaneCrashes/AirplaneComponents/PrintButton.vue
@@ -23,37 +23,42 @@ export default {
         ],
         toPrint: {},
         textpos: 0,
-        fields: [
+        tableFields: [
           { title: "YACSI Number", key: "yacsinumber"},
           { title: "Crash Date", key: "crashdate"},
+          { title: "Date Descriptor", key: "datedescriptor"},
+          { title: "Date Note", key: "datenote"},
           { title: "Aircraft Type", key: "aircrafttype"},
           { title: "Aircraft Registration", key: "aircraftregistration"},
           { title: "Nation", key: "nation"},
           { title: "Militarycivilian", key: "militarycivilian"},
+          { title: "Pilot Last Name", key: "pilotlastname"},
+          { title: "Pilot First Name", key: "pilotfirstname"},
+          { title: "Pilot Rank", key: "pilotrank"},
+          { title: "Souls on Board", key: "soulsonboard"},
+          { title: "Injuries", key: "injuries"},
+          { title: "Fatalities", key: "fatalities"}, 
+          { title: "Location Coordinates", key: "Location"},
+          { title: "Accuracy", key: "accuracy"},
+          { title: "Is In Yukon?", key: "inyukon"},
+          
+        //  { title: "Aircraft Caption", key: "aircraftcaption"},
+        //  { title: "Aircraftaftercrashcaption", key: "aircraftaftercrashcaption"},
+          
+        //  { title: "infoSources", key: "infoSources"},
+        ],
+        textFields:[
           { title: "Crash Location", key: "crashlocation"},
           { title: "Remains on Site", key: "remainsonsite"},
           { title: "Extent of Remains on Site", key: "extentofremainsonsite"},
           { title: "Other Locations of Remains", key: "otherlocationsofremains"},
-        //  { title: "pilot", key: "pilot"},
-          { title: "Fatalities", key: "fatalities"}, 
           { title: "Description of Crashevent", key: "descriptionofcrashevent"},
           { title: "Comments", key: "comments"},
           { title: "Significance of Aircraft", key: "significanceofaircraft"}, 
-        //  { title: "", key: "sources"},
-          { title: "Is In Yukon?", key: "inyukon"},
-          { title: "Souls on Board", key: "soulsonboard"},
-          { title: "Injuries", key: "injuries"},
-          { title: "Date Descriptor", key: "datedescriptor"},
-          { title: "Date Note", key: "datenote"},
-          { title: "Pilot Last Name", key: "pilotlastname"},
-          { title: "Pilot First Name", key: "pilotfirstname"},
-          { title: "Pilot Rank", key: "pilotrank"},
-          { title: "Accuracy", key: "accuracy"},
-        //  { title: "Aircraft Caption", key: "aircraftcaption"},
-        //  { title: "Aircraftaftercrashcaption", key: "aircraftaftercrashcaption"},
-          { title: "Location", key: "Location"},
-        //  { title: "infoSources", key: "infoSources"},
-        ]
+         // { title: "pilot", key: "pilot"},
+        ],
+        generalTable: null,
+
     }),
 
     methods: {
@@ -67,6 +72,10 @@ export default {
             Object.keys(this.toPrint.general).map((key) => {
               this.toPrint.general[key] = this.toPrint.general[key] != null ? this.toPrint.general[key] : '';
             });
+            this.generalTable = this.tableFields.map(x =>{ return [x.title,this.toPrint.general[x.key]]});
+            
+            this.toPrint.infoSources = this.data.infoSources.map(x => {return [x.Source]})
+            console.log(this.toPrint.infoSources);
             /*
             this.toPrint.general.pastNames = this.toPrint.general.pastNames.map(x => {return [x.BoatName]});
 
@@ -85,14 +94,23 @@ export default {
         
         this.printGeneral();
         //this.printPhotos();//not done yet...
-        //this.printHistoricalRecord()
-  
+        this.printTextData();
+        this.printInfoSources();
+
         this.doc.save(`Aircrash_${this.yacsinumber}.pdf`);
       },
       printGeneral(){
-        for(let i = 0; i<this.fields.length; i++){
-          this.addTitle(this.fields[i].title);
-          this.addText(`${this.toPrint.general[this.fields[i].key]}`);
+        this.doc.autoTable({
+        startY: this.textpos,
+        head: [['Field','Data']],
+        body: this.generalTable});
+
+        this.textpos = this.doc.lastAutoTable.finalY+20;
+      },
+      printTextData(){
+        for(let i = 0; i<this.textFields.length; i++){
+          this.addTitle(this.textFields[i].title);
+          this.addText(`${this.toPrint.general[this.textFields[i].key]}`);
           //console.log(`{ title: "", key: "${keys[i]}"},`);
         }
       },
@@ -114,27 +132,11 @@ export default {
       printPhotos(){
 
       },
-      printHistoricalRecord(){
+      printInfoSources(){
         this.doc.autoTable({
         startY: this.textpos,
-        head: [['Historic Record', 'Reference']],
-        body: this.toPrint.historicRecords});
-
-        this.textpos = this.doc.lastAutoTable.finalY+20;
-      },
-      printNames(){
-        this.doc.autoTable({
-        startY: this.textpos,
-        head: [['Name/s:']],
-        body: this.toPrint.general.pastNames});
-
-        this.textpos = this.doc.lastAutoTable.finalY+20;
-      },
-      printOwners(){
-        this.doc.autoTable({
-        startY: this.textpos,
-        head: [['Owner/s:']],
-        body: this.toPrint.general.owners});
+        head: [['Sources']],
+        body: this.toPrint.infoSources});
 
         this.textpos = this.doc.lastAutoTable.finalY+20;
       },
@@ -149,8 +151,5 @@ export default {
                 
       }
     },
-    watch: {
-        
-    }
 }
 </script>
