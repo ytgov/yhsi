f58485c905b7d544be5e271d3106eb41619cbf53
Michael Johnson <michael@icefoganalytics.com>
2021-09-13 12:22:11 -0700

    Wiring up Themes

--- src/web/src/components/Sites/SitesForms/Themes.vue
+++ src/web/src/components/Sites/SitesForms/Themes.vue
@@ -9,99 +9,127 @@
     <v-divider class="mb-5"></v-divider>
     <v-form v-model="valid">
       <div class="row mx-1">
-        <div class="col-md-6"><h3>Themes</h3>
-          <v-alert
-            v-for="(item, i) in fields.themes"
-            :key="`theme-${i + 1}`"
-            outlined
-            color="primary"
-          >
-            <div class="sub-title">Theme {{ i + 1 }}</div>
-            <v-btn
-              icon
-              color="primary"
-              class="top-right-button"
-              @click="removeItem('themes', i)"
-            >
-              <v-icon dark>mdi-minus-circle</v-icon>
-            </v-btn>
-            <v-combobox
-              v-model="item.category"
-              label="Category / Type"
-              dense
-              outlined
-              background-color="white"
-            ></v-combobox>
-          </v-alert>
-          <v-btn color="info" @click="addItem('themes')"> Add theme </v-btn>
-         
-        </div>
-        <div class="col-md-6"> <v-textarea
+        <div class="col-md-12">
+          <v-textarea
             label="YHS Themes"
             v-model="fields.yHSThemes"
             dense
+            rows="4"
             outlined
+            hide-details
             background-color="white"
           ></v-textarea>
-          
         </div>
+
         <div class="col-md-12">
-          <div class="mb-2">Functional Users</div>
-          <v-alert
-            v-for="(item, i) in fields.functionalUses"
-            :key="`theme-${i + 1}`"
-            outlined
-            color="primary"
-          >
-            <div class="sub-title">Funcitonal Use {{ i + 1 }}</div>
-            <v-btn
-              icon
-              color="primary"
-              class="top-right-button"
-              @click="removeItem('functionalUses', i)"
-            >
-              <v-icon dark>mdi-minus-circle</v-icon>
-            </v-btn>
-            <v-row>
-              <v-col cols="4">
-                <v-combobox
-                  label="Use Type"
-                  v-model="item.useType"
-                  dense
-                  outlined
-                  background-color="white"
-                ></v-combobox>
-              </v-col>
-              <v-col cols="8">
-                <v-combobox
-                  v-model="item.category"
-                  label="Functional Category / Type"
-                  dense
-                  outlined
-                  background-color="white"
-                ></v-combobox>
-              </v-col>
-            </v-row>
-          </v-alert>
-          <v-btn color="info" @click="addItem('functionalUses')">
-            Add New
-          </v-btn>
+          <v-card class="default mb-0">
+            <v-card-text>
+              <h3>Themes</h3>
+              <div class="row" v-for="(item, i) of themes" :key="i">
+                <div class="col-md-10">
+                  <v-select
+                    v-model="item.placeThemeId"
+                    :items="themeCategoryOptions"
+                    item-text="display"
+                    item-value="id"
+                    label="Category / Type"
+                    dense
+                    hide-details
+                    outlined
+                    background-color="white"
+                  ></v-select>
+                </div>
+
+                <div class="col-md-2">
+                  <v-btn
+                    color="warning"
+                    x-small
+                    fab
+                    title="Remove"
+                    class="my-0 float-right"
+                    @click="removeName(i)"
+                    ><v-icon>mdi-close</v-icon></v-btn
+                  >
+                </div>
+              </div>
+              <v-btn class="mt-5" color="info" @click="addTheme()">
+                Add Theme
+              </v-btn>
+            </v-card-text>
+          </v-card>
         </div>
+
         <div class="col-md-12">
+          <v-card class="default mb-0">
+            <v-card-text>
+              <h3>Functional Uses</h3>
+              <div class="row" v-for="(item, i) of functionalUses" :key="i">
+                <div class="col-md-10">
+                  <v-row>
+                    <v-col cols="4">
+                      <v-select
+                        label="Use type"
+                        :items="useTypeOptions"
+                        v-model="item.functionalUseType"
+                        item-text="text"
+                        item-value="value"
+                        dense
+                        outlined
+                        hide-details
+                        background-color="white"
+                      ></v-select>
+                    </v-col>
+                    <v-col cols="8">
+                      <v-select
+                        v-model="item.functionalTypeId"
+                        :items="functionalCategoryOptions"
+                        item-text="description"
+                        item-value="id"
+                        label="Functional Category / Type"
+                        dense
+                        outlined
+                        hide-details
+                        background-color="white"
+                      ></v-select>
+                    </v-col>
+                  </v-row>
+                </div>
+
+                <div class="col-md-2">
+                  <v-btn
+                    color="warning"
+                    x-small
+                    fab
+                    title="Remove"
+                    class="my-0 float-right"
+                    @click="removeUse(i)"
+                    ><v-icon>mdi-close</v-icon></v-btn
+                  >
+                </div>
+              </div>
+              <v-btn class="mt-5" color="info" @click="addUse()">
+                Add Functional Use
+              </v-btn>
+            </v-card-text>
+          </v-card>
+        </div>
+        <div class="col-md-6">
           <v-textarea
             label="YHS Current Use"
             v-model="fields.currentUseComment"
             dense
             outlined
             background-color="white"
+            hide-details
           ></v-textarea>
         </div>
-        <div class="col-md-12">
+        <div class="col-md-6">
           <v-textarea
             label="YHS Past Use"
             v-model="fields.yHSPastUse"
             dense
             outlined
+            hide-details
             background-color="white"
           ></v-textarea>
         </div>
@@ -112,7 +140,7 @@
 
 <script>
 import axios from "axios";
-import { PLACE_URL } from "../../../urls";
+import { PLACE_URL, STATIC_URL } from "../../../urls";
 /* Important**, field data that was not found on the swaggerhub api docs provided was assumed to be in development, hence, some placeholder variables were created. */
 export default {
   name: "formThemes",
@@ -122,13 +150,15 @@ export default {
       (v) => !!v || "This input is required",
       (v) => v.length <= 20 || "This input must be less than 20 characters",
     ],
+
+    themes: [],
+    themeCategoryOptions: [],
+    functionalUses: [],
+
+    useTypeOptions: [],
+    functionalCategoryOptions: [],
+
     fields: {
-      /* Placeholder variables below this line **Read above** */
-      themes: [{ category: "" }],
-      functionalUses: [
-        { useType: "text1", category: "" },
-        { useType: "text2", category: "" },
-      ],
       /*Field data from the swaggerhub api docs below this line*/
       currentUseComment: "", //
       yHSPastUse: "", //
@@ -142,27 +172,35 @@ export default {
       .get(`${PLACE_URL}/${id}`)
       .then((resp) => {
         this.fields = resp.data.data;
-        console.log("PLACE", this.fields);
+        this.themes = resp.data.relationships.themes.data;
+        this.functionalUses = resp.data.relationships.functionalUses.data;
       })
       .catch((error) => console.error(error));
+
+    axios.get(`${STATIC_URL}/place-theme`).then((resp) => {
+      this.themeCategoryOptions = resp.data.data;
+    });
+    
+    axios.get(`${STATIC_URL}/functional-use-type`).then((resp) => {
+      this.useTypeOptions = resp.data.data;
+    });
+    
+    axios.get(`${STATIC_URL}/functional-type`).then((resp) => {
+      this.functionalCategoryOptions = resp.data.data;
+    });
   },
   methods: {
-    removeItem(objName, position) {
-      if (position > -1) {
-        this.fields[objName].splice(position, 1);
-      }
+    addTheme() {
+      this.themes.push({});
     },
-    addItem(objName) {
-      switch (
-        objName // Selects which structure to add to the new element of the array
-      ) {
-        case "themes":
-          this.fields[objName].push({ category: "" });
-          break;
-        case "functionalUses":
-          this.fields[objName].push({ useType: "", category: "" });
-          break;
-      }
+    removeTheme(index) {
+      this.themes.splice(index, 1);
+    },
+    addUse() {
+      this.functionalUses.push({});
+    },
+    removeUse(index) {
+      this.functionalUses.splice(index, 1);
     },
   },
 };
