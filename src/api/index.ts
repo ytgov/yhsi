bfd7955a742975c77962714ff962227a71459fe1
Michael Johnson <michael@icefoganalytics.com>
2021-10-21 08:08:40 -0700

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -21,7 +21,7 @@ app.use(
       'block-all-mixed-content': [],
       'font-src': ["'self'", 'https:', 'data:'],
       'frame-ancestors': ["'self'"],
-      'img-src': ["'self'", 'data:'],
+      'img-src': ["'self'", 'data:', 'https:'],
       'object-src': ["'none'"],
       'script-src': ["'self'", 'https://js.arcgis.com',"'unsafe-eval'"], // added https to accomodate esri components?
       'script-src-attr': ["'none'"],
