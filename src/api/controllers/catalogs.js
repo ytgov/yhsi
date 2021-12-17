4f459894c1cc55f5e74336974b151d85c0389a68
Angel Gomez <angel@bizont.ca>
2021-12-17 16:01:20 -0700

    Sites catalog endpoint finished

--- src/api/controllers/catalogs.js
+++ src/api/controllers/catalogs.js
@@ -103,31 +103,28 @@ router.get('/community', authenticateToken, async (req, res) => {
   
     const db = req.app.get('db');
   
-    const { page = 0, limit = 10, textToMatch = '', sortBy = 'Id', sort = 'asc' } = req.query;
+    const { page = 0, limit = 10, textToMatch = '', sortBy = 'Name', sort = 'asc' } = req.query;
     const offset = (page*limit) || 0;
-    let counter = 0;
 
-    let types = [];
+    let sites = [];
 
       if (textToMatch) {
-        counter = await db.from('Boat.Type')
-        .where('Boat.Type.Type', 'like', `%${textToMatch}%`)
-        .count('Id', {as: 'count'});
     
-        types = await db.from('Boat.Type')
-        .where('Boat.Type.Type', 'like', `%${textToMatch}%`)
+        sites = await db.select('*')
+        .from('Place.Place')
+        .where('Place.Place.Name', 'like', `%${textToMatch}%`)
         .orderBy(`${sortBy}`,`${sort}`)
         .limit(limit).offset(offset);
     
       } else {
-        counter = await db.from('Boat.Type').count('Id', {as: 'count'});
         
-        types =  await db.select('*')
-        .from('Boat.Type').orderBy(`${sortBy}`,`${sort}`)
+        sites =  await db.select('*')
+        .from('Boat.Place')
+        .orderBy(`${sortBy}`,`${sort}`)
         .limit(limit).offset(offset);;
     
       }
     
-      res.status(200).send({ count: counter[0].count, body: types });
+      res.status(200).send({ body: sites });
   });
   module.exports = router
