e559c8ffbac5e4ce139a84c554e4a61733e9ebfc
Max Parker <maxrparker@gmail.com>
2022-01-17 13:52:33 -0800

    Fixed typescript errors. Working on NPM build error

--- src/api/routes/aircrash-router.ts
+++ src/api/routes/aircrash-router.ts
@@ -1,212 +1,249 @@
-import { Request, Response } from "express";
-const express = require("express");
-import { DB_CONFIG } from "../config"
-const knex = require("knex");
-import { ReturnValidationErrors } from "../middleware";
-import { param, query } from "express-validator";
+import { Request, Response } from 'express';
+const express = require('express');
+import { DB_CONFIG } from '../config';
+const knex = require('knex');
+import { ReturnValidationErrors } from '../middleware';
+import { param, query } from 'express-validator';
 
 export const aircrashRouter = express.Router();
 const db = knex(DB_CONFIG);
 
-
-aircrashRouter.get('/',
-  [query("page").default(0).isInt(), query("limit").default(10).isInt({ gt: 0 })], ReturnValidationErrors,
-  async (req: Request, res: Response) => {
-    /* const permissions = req.decodedToken['yg-claims'].permissions;
+aircrashRouter.get(
+	'/',
+	[
+		query('page').default(0).isInt(),
+		query('limit').default(10).isInt({ gt: 0 }),
+	],
+	ReturnValidationErrors,
+	async (req: Request, res: Response) => {
+		/* const permissions = req.decodedToken['yg-claims'].permissions;
     if (!permissions.includes('view')) res.sendStatus(403);
   
     const db = req.app.get('db'); */
 
-    const { textToMatch = '', sortBy = 'yacsinumber', sort = 'asc' } = req.query;
-    const page = parseInt(req.query.page as string);
-    const limit = parseInt(req.query.limit as string);
-    const offset = (page * limit) || 0;
-    let counter = [{ count: 0 }];
-    let aircrashes = [];
+		const {
+			textToMatch = '',
+			sortBy = 'yacsinumber',
+			sort = 'asc',
+		} = req.query;
+		const page = parseInt(req.query.page as string);
+		const limit = parseInt(req.query.limit as string);
+		const offset = page * limit || 0;
+		let counter = [{ count: 0 }];
+		let aircrashes = [];
 
-    if (textToMatch) {
-      counter = await db.from('dbo.vAircrash')
-        .where('yacsinumber', 'like', `%${textToMatch}%`)
-        .orWhere('crashdate', 'like', `%${textToMatch}%`)
-        .orWhere('aircrafttype', 'like', `%${textToMatch}%`)
-        .orWhere('aircraftregistration', 'like', `%${textToMatch}%`)
-        .orWhere('nation', 'like', `%${textToMatch}%`)
-        .orWhere('militarycivilian', 'like', `%${textToMatch}%`)
-        .orWhere('crashlocation', 'like', `%${textToMatch}%`)
-        .orWhere('pilot', 'like', `%${textToMatch}%`)
-        .orWhere('soulsonboard', 'like', `%${textToMatch}%`)
-        .orWhere('injuries', 'like', `%${textToMatch}%`)
-        .orWhere('fatalities', 'like', `%${textToMatch}%`)
-        .count('yacsinumber', { as: 'count' });
+		if (textToMatch) {
+			counter = await db
+				.from('dbo.vAircrash')
+				.where('yacsinumber', 'like', `%${textToMatch}%`)
+				.orWhere('crashdate', 'like', `%${textToMatch}%`)
+				.orWhere('aircrafttype', 'like', `%${textToMatch}%`)
+				.orWhere('aircraftregistration', 'like', `%${textToMatch}%`)
+				.orWhere('nation', 'like', `%${textToMatch}%`)
+				.orWhere('militarycivilian', 'like', `%${textToMatch}%`)
+				.orWhere('crashlocation', 'like', `%${textToMatch}%`)
+				.orWhere('pilot', 'like', `%${textToMatch}%`)
+				.orWhere('soulsonboard', 'like', `%${textToMatch}%`)
+				.orWhere('injuries', 'like', `%${textToMatch}%`)
+				.orWhere('fatalities', 'like', `%${textToMatch}%`)
+				.count('yacsinumber', { as: 'count' });
 
-      aircrashes = await db.select('*')
-        .from('dbo.vAircrash')
-        .where('yacsinumber', 'like', `%${textToMatch}%`)
-        .orWhere('crashdate', 'like', `%${textToMatch}%`)
-        .orWhere('aircrafttype', 'like', `%${textToMatch}%`)
-        .orWhere('aircraftregistration', 'like', `%${textToMatch}%`)
-        .orWhere('nation', 'like', `%${textToMatch}%`)
-        .orWhere('militarycivilian', 'like', `%${textToMatch}%`)
-        .orWhere('crashlocation', 'like', `%${textToMatch}%`)
-        .orWhere('pilot', 'like', `%${textToMatch}%`)
-        .orWhere('soulsonboard', 'like', `%${textToMatch}%`)
-        .orWhere('injuries', 'like', `%${textToMatch}%`)
-        .orWhere('fatalities', 'like', `%${textToMatch}%`)
-        //.orderBy('yacsinumber', 'asc')
-        .orderBy(`${sortBy}`, `${sort}`)
-        .limit(limit).offset(offset);
+			aircrashes = await db
+				.select('*')
+				.from('dbo.vAircrash')
+				.where('yacsinumber', 'like', `%${textToMatch}%`)
+				.orWhere('crashdate', 'like', `%${textToMatch}%`)
+				.orWhere('aircrafttype', 'like', `%${textToMatch}%`)
+				.orWhere('aircraftregistration', 'like', `%${textToMatch}%`)
+				.orWhere('nation', 'like', `%${textToMatch}%`)
+				.orWhere('militarycivilian', 'like', `%${textToMatch}%`)
+				.orWhere('crashlocation', 'like', `%${textToMatch}%`)
+				.orWhere('pilot', 'like', `%${textToMatch}%`)
+				.orWhere('soulsonboard', 'like', `%${textToMatch}%`)
+				.orWhere('injuries', 'like', `%${textToMatch}%`)
+				.orWhere('fatalities', 'like', `%${textToMatch}%`)
+				//.orderBy('yacsinumber', 'asc')
+				.orderBy(`${sortBy}`, `${sort}`)
+				.limit(limit)
+				.offset(offset);
+		} else {
+			counter = await db
+				.from('dbo.vAircrash')
+				.count('yacsinumber', { as: 'count' });
 
-    } else {
-      counter = await db.from('dbo.vAircrash').count('yacsinumber', { as: 'count' })
+			aircrashes = await db
+				.select('*')
+				.from('dbo.vAircrash')
+				//.orderBy('dbo.vAircrash.yacsinumber', 'asc')
+				.orderBy(`${sortBy}`, `${sort}`)
+				.limit(limit)
+				.offset(offset);
+		}
 
-      aircrashes = await db.select('*')
-        .from('dbo.vAircrash')
-        //.orderBy('dbo.vAircrash.yacsinumber', 'asc')
-        .orderBy(`${sortBy}`, `${sort}`)
-        .limit(limit).offset(offset);
-    }
+		res.status(200).send({ count: counter[0].count, body: aircrashes });
+	}
+);
 
-    res.status(200).send({ count: counter[0].count, body: aircrashes });
-  });
-
-aircrashRouter.get('/:aircrashId',
-  [param("aircrashId").notEmpty()], ReturnValidationErrors,
-  async (req: Request, res: Response) => {
-    /*  const db = req.app.get('db');
+aircrashRouter.get(
+	'/:aircrashId',
+	[param('aircrashId').notEmpty()],
+	ReturnValidationErrors,
+	async (req: Request, res: Response) => {
+		/*  const db = req.app.get('db');
    
      const permissions = req.decodedToken['yg-claims'].permissions;
      if (!permissions.includes('view')) res.sendStatus(403); */
 
-    const { aircrashId } = req.params;
-    const aircrash = await db.select('*')
-      .from('dbo.vAircrash')
-      .where('dbo.vAircrash.yacsinumber', aircrashId)
-      .first();
+		const { aircrashId } = req.params;
+		const aircrash = await db
+			.select('*')
+			.from('dbo.vAircrash')
+			.where('dbo.vAircrash.yacsinumber', aircrashId)
+			.first();
 
-    aircrash.infoSources = await db.select('*')
-      .from('AirCrash.InfoSource')
-      .where('AirCrash.InfoSource.YACSINumber', aircrashId);
+		aircrash.infoSources = await db
+			.select('*')
+			.from('AirCrash.InfoSource')
+			.where('AirCrash.InfoSource.YACSINumber', aircrashId);
 
-    if (!aircrash) {
-      res.status(403).send('Airplane crash id not found');
-      return
-    };
+		if (!aircrash) {
+			res.status(403).send('Airplane crash id not found');
+			return;
+		}
 
-    res.status(200).send(aircrash);
-  });
+		res.status(200).send(aircrash);
+	}
+);
 
-aircrashRouter.put('/:aircrashId',
-  [param("aircrashId").notEmpty()], ReturnValidationErrors,
-  async (req: Request, res: Response) => {
-    /*  const db = req.app.get('db');
+aircrashRouter.put(
+	'/:aircrashId',
+	[param('aircrashId').notEmpty()],
+	ReturnValidationErrors,
+	async (req: Request, res: Response) => {
+		/*  const db = req.app.get('db');
      const permissions = req.decodedToken['yg-claims'].permissions;
      if (!permissions.includes('edit')) res.sendStatus(403); */
 
-    const { aircrashId } = req.params;
+		const { aircrashId } = req.params;
 
-    const { aircrash = {},
-      removedInfoSources,
-      newInfoSources,
-      editedInfoSources } = req.body;
+		const {
+			aircrash = {},
+			removedInfoSources,
+			newInfoSources,
+			editedInfoSources,
+		} = req.body;
 
-    //make the update
-    await db('AirCrash.AirCrash')
-      .update(aircrash)
-      .where('AirCrash.AirCrash.yacsinumber', aircrashId);
+		//make the update
+		await db('AirCrash.AirCrash')
+			.update(aircrash)
+			.where('AirCrash.AirCrash.yacsinumber', aircrashId);
 
+		//Add the new info sources (in progress)
+		console.log(newInfoSources);
+		await db
+			.insert(
+				newInfoSources.map((source: any) => ({
+					YACSINumber: aircrashId,
+					...source,
+				}))
+			)
+			.into('AirCrash.InfoSource')
+			.then((rows: any) => {
+				return rows;
+			});
 
-    //Add the new info sources (in progress)
-    console.log(newInfoSources);
-    await db.insert(newInfoSources.map((source: any) => ({ YACSINumber: aircrashId, ...source })))
-      .into('AirCrash.InfoSource')
-      .then(rows => {
-        return rows;
-      });
+		//remove the previous owners (DONE)
+		for (const obj of removedInfoSources) {
+			await db('AirCrash.InfoSource')
+				.where('AirCrash.InfoSource.Id', obj.Id)
+				.del();
+		}
 
-    //remove the previous owners (DONE)
-    for (const obj of removedInfoSources) {
-      await db('AirCrash.InfoSource')
-        .where('AirCrash.InfoSource.Id', obj.Id)
-        .del();
-    }
+		//update the info sources (DONE)
+		for (const obj of editedInfoSources) {
+			await db('AirCrash.InfoSource')
+				.update({ Source: obj.Source })
+				.where('AirCrash.InfoSource.Id', obj.Id);
+		}
 
-    //update the info sources (DONE)
-    for (const obj of editedInfoSources) {
-      await db('AirCrash.InfoSource')
-        .update({ Source: obj.Source })
-        .where('AirCrash.InfoSource.Id', obj.Id);
-    }
+		res.status(200).send({ message: 'success' });
+	}
+);
 
-    res.status(200).send({ message: 'success' });
-  });
-
-aircrashRouter.post('/',
-  async (req: Request, res: Response) => {
-    /* const db = req.app.get('db');
+aircrashRouter.post('/', async (req: Request, res: Response) => {
+	/* const db = req.app.get('db');
     const permissions = req.decodedToken['yg-claims'].permissions;
     if (!permissions.includes('create')) res.sendStatus(403); */
 
-    const { aircrash = {}, newInfoSources } = req.body;
-    /*
+	const { aircrash = {}, newInfoSources } = req.body;
+	/*
       const response = await db.insert(aircrash)
         .into('AirCrash.AirCrash')
         .returning('*');
     */
-    const exists = await db.select('*')
-      .from('dbo.vAircrash')
-      .where('dbo.vAircrash.yacsinumber', aircrash.yacsinumber)
-      .first();
+	const exists = await db
+		.select('*')
+		.from('dbo.vAircrash')
+		.where('dbo.vAircrash.yacsinumber', aircrash.yacsinumber)
+		.first();
 
-    if (exists) {
-      //this is a 409 conflict, i might change the status to 409 after some tests
-      res.status(409).send('The YACSI Number already exists');
-      return;
-    }
+	if (exists) {
+		//this is a 409 conflict, i might change the status to 409 after some tests
+		res.status(409).send('The YACSI Number already exists');
+		return;
+	}
 
-    const response = await db.insert(aircrash)
-      .into('AirCrash.AirCrash')
-      .returning('*')
-      .then(async rows => {
-        const newAirCrash = rows[0];
+	const response = await db
+		.insert(aircrash)
+		.into('AirCrash.AirCrash')
+		.returning('*')
+		.then(async (rows: any) => {
+			const newAirCrash = rows[0];
 
-        if (newInfoSources.length) {
-          const finalInfoSources = newInfoSources.map((source: any) => ({ YACSINumber: newAirCrash.YACSINumber, ...source }))
+			if (newInfoSources.length) {
+				const finalInfoSources = newInfoSources.map((source: any) => ({
+					YACSINumber: newAirCrash.YACSINumber,
+					...source,
+				}));
 
-          await db.insert(finalInfoSources)
-            .into('AirCrash.InfoSource')
-            .returning('*')
-            .then(rows => {
-              return rows;
-            });
-        }
+				await db
+					.insert(finalInfoSources)
+					.into('AirCrash.InfoSource')
+					.returning('*')
+					.then((rows: any) => {
+						return rows;
+					});
+			}
 
-        return newAirCrash;
-      });
+			return newAirCrash;
+		});
 
-    res.status(200).send(response);
+	res.status(200).send(response);
+});
 
-  });
-
-
-aircrashRouter.get('/available_yacsi/:YACSINumber',
-  [param("YACSINumber").notEmpty()], ReturnValidationErrors,
-  async (req: Request, res: Response) => {
-    /* const permissions = req.decodedToken['yg-claims'].permissions;
+aircrashRouter.get(
+	'/available_yacsi/:YACSINumber',
+	[param('YACSINumber').notEmpty()],
+	ReturnValidationErrors,
+	async (req: Request, res: Response) => {
+		/* const permissions = req.decodedToken['yg-claims'].permissions;
     if (!permissions.includes('view')) res.sendStatus(403);
   
     const db = req.app.get('db'); */
 
-    const { YACSINumber } = req.params;
-    let available = true;
+		const { YACSINumber } = req.params;
+		let available = true;
 
-    const exists = await db.select('*')
-      .from('dbo.vAircrash')
-      .where('dbo.vAircrash.yacsinumber', YACSINumber)
-      .first();
+		const exists = await db
+			.select('*')
+			.from('dbo.vAircrash')
+			.where('dbo.vAircrash.yacsinumber', YACSINumber)
+			.first();
 
-    if (exists) {
-      available = false;
-    }
+		if (exists) {
+			available = false;
+		}
 
-    res.status(200).send({ available });
-  });
+		res.status(200).send({ available });
+	}
+);
--- src/api/services/static-service.ts
+++ src/api/services/static-service.ts
@@ -1,174 +1,239 @@
-import { knex, Knex } from "knex";
-import { Community, FirstNation, FunctionalType, OriginalMedia, PhotoOwner, PhotoProject, PlaceTheme, Statute } from "../data";
+import Knex from 'knex';
+import {
+	Community,
+	FirstNation,
+	FunctionalType,
+	OriginalMedia,
+	PhotoOwner,
+	PhotoProject,
+	PlaceTheme,
+	Statute,
+} from '../data';
 
 export class StaticService {
+	private knex: Knex;
 
-    private knex: Knex;
+	constructor(config: Knex.Config<any>) {
+		this.knex = Knex(config);
+	}
 
-    constructor(config: Knex.Config<any>) {
-        this.knex = knex(config);
-    }
+	async getCommunities(): Promise<Array<Community>> {
+		return this.knex<Community>('Community').select('id', 'name');
+	}
 
-    async getCommunities(): Promise<Array<Community>> {
-        return this.knex<Community>("Community").select("id", "name")
-    }
+	async getFirstNations(): Promise<Array<FirstNation>> {
+		return this.knex<FirstNation>('FirstNation').select('id', 'description');
+	}
 
-    async getFirstNations(): Promise<Array<FirstNation>> {
-        return this.knex<FirstNation>("FirstNation").select("id", "description")
-    }
+	async getFunctionalTypes(): Promise<Array<FunctionalType>> {
+		return this.knex<FunctionalType>('FunctionalType')
+			.select('id', 'description')
+			.orderBy('description');
+	}
 
-    async getFunctionalTypes(): Promise<Array<FunctionalType>> {
-        return this.knex<FunctionalType>("FunctionalType").select("id", "description").orderBy("description")
-    }
+	async getOriginalMedias(): Promise<Array<OriginalMedia>> {
+		return this.knex<OriginalMedia>('OriginalMedia').select('id', 'type');
+	}
 
-    async getOriginalMedias(): Promise<Array<OriginalMedia>> {
-        return this.knex<OriginalMedia>("OriginalMedia").select("id", "type")
-    }
+	async getPhotoOwners(): Promise<Array<PhotoOwner>> {
+		return this.knex<PhotoOwner>('PhotoOwner').select(
+			'id',
+			'name',
+			'email',
+			'address',
+			'telephone',
+			'contactPerson'
+		);
+	}
 
-    async getPhotoOwners(): Promise<Array<PhotoOwner>> {
-        return this.knex<PhotoOwner>("PhotoOwner").select("id", "name", "email", "address", "telephone", "contactPerson")
-    }
+	async getPhotoOwner(id: number): Promise<PhotoOwner | undefined> {
+		return this.knex<PhotoOwner>('PhotoOwner')
+			.where({ id })
+			.select('id', 'name', 'email', 'address', 'telephone', 'contactPerson')
+			.first();
+	}
 
-    async getPhotoOwner(id: number): Promise<PhotoOwner | undefined> {
-        return this.knex<PhotoOwner>("PhotoOwner").where({ id }).select("id", "name", "email", "address", "telephone", "contactPerson").first();
-    }
+	async addPhotoOwner(owner: PhotoOwner): Promise<PhotoOwner | undefined> {
+		return this.knex('PhotoOwner')
+			.insert(owner)
+			.returning<PhotoOwner>([
+				'id',
+				'name',
+				'email',
+				'address',
+				'telephone',
+				'contactPerson',
+			]);
+	}
 
-    async addPhotoOwner(owner: PhotoOwner): Promise<PhotoOwner | undefined> {
-        return this.knex("PhotoOwner").insert(owner).returning<PhotoOwner>(["id", "name", "email", "address", "telephone", "contactPerson"]);
-    }
+	async updatePhotoOwner(
+		id: number,
+		item: PhotoOwner
+	): Promise<PhotoOwner | undefined> {
+		return this.knex('PhotoOwner')
+			.where({ id: id })
+			.update(item)
+			.returning<PhotoOwner>([
+				'id',
+				'name',
+				'email',
+				'address',
+				'telephone',
+				'contactPerson',
+			]);
+	}
 
-    async updatePhotoOwner(id: number, item: PhotoOwner): Promise<PhotoOwner | undefined> {
-        return this.knex("PhotoOwner").where({ id: id }).update(item).returning<PhotoOwner>(["id", "name", "email", "address", "telephone", "contactPerson"]);
-    }
+	async deletePhotoOwner(id: number): Promise<any> {
+		return this.knex('PhotoOwner').where({ id }).delete();
+	}
 
-    async deletePhotoOwner(id: number): Promise<any> {
-        return this.knex("PhotoOwner").where({ id }).delete();
-    }
+	async getPhotoProjects(): Promise<Array<PhotoProject>> {
+		return this.knex<PhotoProject>('PhotoProject').select(
+			'id',
+			'name',
+			'permit',
+			'year',
+			'section'
+		);
+	}
 
-    async getPhotoProjects(): Promise<Array<PhotoProject>> {
-        return this.knex<PhotoProject>("PhotoProject").select("id", "name", "permit", "year", "section")
-    }
+	async getPhotoProject(id: number): Promise<PhotoProject | undefined> {
+		return this.knex<PhotoProject>('PhotoProject')
+			.where({ id })
+			.select('id', 'name', 'permit', 'year', 'section')
+			.first();
+	}
 
-    async getPhotoProject(id: number): Promise<PhotoProject | undefined> {
-        return this.knex<PhotoProject>("PhotoProject").where({ id }).select("id", "name", "permit", "year", "section").first();
-    }
+	async addPhotoProject(item: PhotoProject): Promise<PhotoProject | undefined> {
+		return this.knex('PhotoProject')
+			.insert(item)
+			.returning<PhotoProject>(['id', 'name', 'permit', 'year', 'section']);
+	}
 
-    async addPhotoProject(item: PhotoProject): Promise<PhotoProject | undefined> {
-        return this.knex("PhotoProject").insert(item).returning<PhotoProject>(["id", "name", "permit", "year", "section"]);
-    }
+	async updatePhotoProject(
+		id: number,
+		item: PhotoProject
+	): Promise<PhotoProject | undefined> {
+		return this.knex('PhotoProject')
+			.where({ id: id })
+			.update(item)
+			.returning<PhotoProject>(['id', 'name', 'permit', 'year', 'section']);
+	}
 
-    async updatePhotoProject(id: number, item: PhotoProject): Promise<PhotoProject | undefined> {
-        return this.knex("PhotoProject").where({ id: id }).update(item).returning<PhotoProject>(["id", "name", "permit", "year", "section"]);
-    }
+	async deletePhotoProject(id: number): Promise<any> {
+		return this.knex('PhotoProject').where({ id }).delete();
+	}
 
-    async deletePhotoProject(id: number): Promise<any> {
-        return this.knex("PhotoProject").where({ id }).delete();
-    }
+	async getPlaceThemes(): Promise<Array<PlaceTheme>> {
+		return this.knex<PlaceTheme>('PlaceTheme').select('id', 'category', 'type');
+	}
 
-    async getPlaceThemes(): Promise<Array<PlaceTheme>> {
-        return this.knex<PlaceTheme>("PlaceTheme").select("id", "category", "type");
-    }
+	async getStatutes(): Promise<Array<Statute>> {
+		return this.knex<Statute>('Statute').select(
+			'id',
+			'recognitionAuthority',
+			'recognitionType',
+			'description',
+			'allStatute'
+		);
+	}
 
-    async getStatutes(): Promise<Array<Statute>> {
-        return this.knex<Statute>("Statute").select("id", "recognitionAuthority", "recognitionType", "description", "allStatute");
-    }
+	getJurisdictions(): GenericEnum[] {
+		return [
+			{ value: 0, text: 'None Selected' },
+			{ value: 1, text: 'Yukon' },
+			{ value: 2, text: 'Federal' },
+		];
+	}
 
-    getJurisdictions(): GenericEnum[] {
-        return [
-            { value: 0, text: "None Selected" },
-            { value: 1, text: "Yukon" },
-            { value: 2, text: "Federal" }
-        ];
-    }
+	getOwnerConsents(): GenericEnum[] {
+		return [
+			{ value: 0, text: 'None Selected' },
+			{ value: 1, text: 'Consent' },
+			{ value: 2, text: 'No Response' },
+			{ value: 3, text: 'Objection' },
+		];
+	}
 
-    getOwnerConsents(): GenericEnum[] {
-        return [
-            { value: 0, text: "None Selected" },
-            { value: 1, text: "Consent" },
-            { value: 2, text: "No Response" },
-            { value: 3, text: "Objection" }
-        ];
-    }
+	getCategories(): GenericEnum[] {
+		return [
+			{ value: 0, text: 'None Selected' },
+			{ value: 1, text: 'Building' },
+			{ value: 2, text: 'District' },
+			{ value: 3, text: 'Place' },
+			{ value: 4, text: 'Structure' },
+		];
+	}
 
-    getCategories(): GenericEnum[] {
-        return [
-            { value: 0, text: "None Selected" },
-            { value: 1, text: "Building" },
-            { value: 2, text: "District" },
-            { value: 3, text: "Place" },
-            { value: 4, text: "Structure" }
-        ];
-    }
+	getDesignationTypes(): GenericEnum[] {
+		return [
+			{ value: 4, text: 'Federal' },
+			{ value: 2, text: 'Municipal' },
+			{ value: 3, text: 'Territorial' },
+			{ value: 5, text: 'World' },
+			{ value: 0, text: 'Not Designated' },
+		];
+	}
 
-    getDesignationTypes(): GenericEnum[] {
-        return [
-            { value: 4, text: "Federal" },
-            { value: 2, text: "Municipal" },
-            { value: 3, text: "Territorial" },
-            { value: 5, text: "World" },
-            { value: 0, text: "Not Designated" }
-        ];
-    }
+	getConditions(): GenericEnum[] {
+		return [
+			{ value: 0, text: 'Not Applicable' },
+			{ value: 3, text: 'Poor' },
+			{ value: 1, text: 'Fair' },
+			{ value: 2, text: 'Good' },
+		];
+	}
 
-    getConditions(): GenericEnum[] {
-        return [
-            { value: 0, text: "Not Applicable" },
-            { value: 3, text: "Poor" },
-            { value: 1, text: "Fair" },
-            { value: 2, text: "Good" }
-        ];
-    }
+	getCoordinateDeterminations(): GenericEnum[] {
+		return [
+			{ value: 4, text: 'Digital Maps' },
+			{ value: 5, text: 'Geocoding' },
+			{ value: 1, text: 'GPS' },
+			{ value: 2, text: 'Paper Maps' },
+			{ value: 3, text: 'Unknown' },
+		];
+	}
 
-    getCoordinateDeterminations(): GenericEnum[] {
-        return [
-            { value: 4, text: "Digital Maps" },
-            { value: 5, text: "Geocoding" },
-            { value: 1, text: "GPS" },
-            { value: 2, text: "Paper Maps" },
-            { value: 3, text: "Unknown" }
-        ];
-    }
+	getSiteStatuses(): GenericEnum[] {
+		return [
+			{ value: 1, text: 'Standing' },
+			{ value: 2, text: 'Demolished' },
+			{ value: 3, text: 'Burned' },
+			{ value: 4, text: 'Moved' },
+			{ value: 5, text: 'Dimantled' },
+			{ value: 6, text: 'Reconstruction' },
+		];
+	}
 
-    getSiteStatuses(): GenericEnum[] {
-        return [
-            { value: 1, text: "Standing" },
-            { value: 2, text: "Demolished" },
-            { value: 3, text: "Burned" },
-            { value: 4, text: "Moved" },
-            { value: 5, text: "Dimantled" },
-            { value: 6, text: "Reconstruction" }
-        ];
-    }
+	getRecordTypes(): GenericEnum[] {
+		return [
+			{ value: 1, text: 'CRHP' },
+			{ value: 2, text: 'WHBR' },
+		];
+	}
 
-    getRecordTypes(): GenericEnum[] {
-        return [
-            { value: 1, text: "CRHP" },
-            { value: 2, text: "WHBR" }
-        ];
-    }
+	getSiteCategories(): GenericEnum[] {
+		return [
+			{ value: 1, text: 'Architecture' },
+			{ value: 4, text: 'First Nation' },
+			{ value: 5, text: 'Gravesite' },
+			{ value: 2, text: 'Industrial' },
+			{ value: 3, text: 'Landscape' },
+		];
+	}
 
-    getSiteCategories(): GenericEnum[] {
-        return [
-            { value: 1, text: "Architecture" },
-            { value: 4, text: "First Nation" },
-            { value: 5, text: "Gravesite" },
-            { value: 2, text: "Industrial" },
-            { value: 3, text: "Landscape" }
-        ];
-    }
-
-    getContributingResourceTypes(): GenericEnum[] {
-        return [
-            { value: 4, text: "Archaeological" },
-            { value: 2, text: "Building" },
-            { value: 9, text: "Collection" },
-            { value: 3, text: "Landscapes" },
-            { value: 1, text: "Structure" }
-        ];
-    }
+	getContributingResourceTypes(): GenericEnum[] {
+		return [
+			{ value: 4, text: 'Archaeological' },
+			{ value: 2, text: 'Building' },
+			{ value: 9, text: 'Collection' },
+			{ value: 3, text: 'Landscapes' },
+			{ value: 1, text: 'Structure' },
+		];
+	}
 }
 
 export interface GenericEnum {
-    value: number;
-    text: string;
+	value: number;
+	text: string;
 }
