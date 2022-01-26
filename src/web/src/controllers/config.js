a182c5ffd57e95502a58405fbba0c86d43e0b168
allanglez <edgar@bizont.ca>
2022-01-26 15:47:48 -0700

    refactor on auth to secure apis

--- src/web/src/controllers/config.js
+++ src/web/src/controllers/config.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 // const BASE_URL =  'https://api.gov.yk.ca/heritage/api/';
 //const BASE_URL = `${apiBaseUrl}/api`;
-const BASE_URL = `http://localhost:4125/api/`;
+const BASE_URL = `http://localhost:3000/api/`;
 const TEST_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ5Zy1jbGFpbXMiOnsicGVybWlzc2lvbnMiOlsidmlldyIsImNyZWF0ZSIsImVkaXQiXX0sInN1YiI6IjEyMzQ1Njc4OTAiLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9.BDa7etuFx-QoqTHxEwOpBaWdsbupbrL4YszziI_W7to'
 axios.defaults.headers.common['Authorization'] = `Bearer ${TEST_TOKEN}`;
 axios.defaults.headers.common['Ocp-Apim-Subscription-Key'] = '5d23d587d6674b88b29349f12da7a406';
@@ -12,7 +12,7 @@ axios.defaults.headers.common['Ocp-Apim-Subscription-Key'] = '5d23d587d6674b88b2
 const api = axios.create({
   baseURL: BASE_URL,
   timeout: 240000,
-  //withCredentials: true,
+  withCredentials: true,
 });
 //axios.defaults.withCredentials = true;
 
