f634bfc660a3efa3b0ef44bdf978d9d5f397a40b
Max Parker <maxrparker@gmail.com>
2022-02-14 12:19:29 -0700

    Missed some

--- src/api/routes/ytplace-router.ts
+++ src/api/routes/ytplace-router.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express, { Request, Response } from 'express';
 import { DB_CONFIG } from '../config';
 import { body, check, param, query, validationResult } from 'express-validator';
@@ -33,16 +32,6 @@ import {
 	YtPlace,
 } from '../data';
 import { ReturnValidationErrors } from '../middleware';
-=======
-import express, { Request, Response } from "express";
-import { DB_CONFIG } from "../config"
-import { body, check, param, query, validationResult } from "express-validator";
-import { PhotoService, YtPlaceService, SortDirection, SortStatement, StaticService } from "../services";
-import { AlternateName, HistoricalPattern, Name, Place, PlaceType, Dates, PLACE_FIELDS, ConstructionPeriod, Theme, FunctionalUse, Association, FnAssociation, FirstNationName, Ownership, PreviousOwnership, Photo, WebLink, RevisionLog, Contact, Description, YtPlace } from "../data";
-import { ReturnValidationErrors } from "../middleware";
-import moment from "moment";
-import knex from "knex";
->>>>>>> 68a9650 (Finished create/edit places form and print screen. Only the photos widget is left)
 
 const ytPlaceService = new YtPlaceService(DB_CONFIG);
 const staticService = new StaticService(DB_CONFIG);
@@ -50,9 +39,7 @@ const photoService = new PhotoService(DB_CONFIG);
 const PAGE_SIZE = 10;
 
 export const ytPlaceRouter = express.Router();
-const db = knex(DB_CONFIG);
 
-<<<<<<< HEAD
 ytPlaceRouter.get(
 	'/',
 	[query('page').default(1).isInt({ gt: 0 })],
@@ -305,187 +292,6 @@ ytPlaceRouter.put(
 		);
 
 		/*for (let on of oldNames) {
-=======
-ytPlaceRouter.get("/",
-    [query("page").default(1).isInt({ gt: 0 })], ReturnValidationErrors,
-    async (req: Request, res: Response) => {
-
-
-        let page = parseInt(req.query.page as string);
-        let skip = (page - 1) * PAGE_SIZE;
-        let take = PAGE_SIZE;
-
-        let list = await ytPlaceService.getAll(skip, take)
-            .then(data => data)
-            .catch((err) => { console.error("Database Error", err); return undefined; });
-
-        let item_count = await ytPlaceService.getPlaceCount()
-            .then(data => data)
-            .catch((err) => { console.error("Database Error", err); return 0; });
-
-        let page_count = Math.ceil(item_count / PAGE_SIZE);
-
-        if (list) {
-            return res.json({ data: list, meta: { page, page_size: PAGE_SIZE, item_count, page_count } });
-        }
-
-        return res.status(500).send("Error")
-    });
-
-ytPlaceRouter.post("/search", [body("page").isInt().default(1)],
-    async (req: Request, res: Response) => {
-        let { query, sortBy, sortDesc, page, itemsPerPage } = req.body;
-        let sort = new Array<SortStatement>();
-
-        sortBy.forEach((s: string, i: number) => {
-            sort.push({ field: s, direction: sortDesc[i] ? SortDirection.ASCENDING : SortDirection.DESCENDING })
-        })
-
-        let skip = (page - 1) * itemsPerPage;
-        let take = itemsPerPage;
-        let results = await ytPlaceService.doSearch(query, sort, page, itemsPerPage, skip, take);
-
-        for (let place of results.data) {    
-            place.placeTypes = await ytPlaceService.getPlaceTypesFor(place.id);          
-            place.placeTypes = combine(place.placeTypes, await ytPlaceService.getPlaceTypeNames(), 'id', 'placeTypeLookupId', "placeType", "placeType");   
-            place.firstNationNames = await ytPlaceService.getFirstNationNamesFor(place.id);
-            place.alternateNames = await ytPlaceService.getAlternateNamesFor(place.id);
-        }
-
-        res.json(results);
-    });
-
-ytPlaceRouter.get("/:id",
-    [
-        check("id").notEmpty()
-    ], ReturnValidationErrors,
-    async (req: Request, res: Response) => {
-        let id = parseInt(req.params.id);
-        let fnList = await staticService.getFirstNations();
-
-        await ytPlaceService.getById(id)
-            .then(async (place) => {
-                if (place) {
-                    let placeTypes = await ytPlaceService.getPlaceTypesFor(place.id);          
-                    placeTypes = combine(placeTypes, await ytPlaceService.getPlaceTypeNames(), 'id', 'placeTypeLookupId', "placeType", "placeType");   
-                    let fnNames = await ytPlaceService.getFirstNationNamesFor(place.id);
-                    let altNames = await ytPlaceService.getAlternateNamesFor(place.id);
-                    let histories = await ytPlaceService.getPlaceHistoriesFor(place.id);      
- 
-                    let fnAssociations = await ytPlaceService.getFNAssociationsFor(place.id);
-                    fnAssociations = combine(fnAssociations, fnList, "firstNationId", "id", "description", "firstNationDescription" );   
-                    fnAssociations = combine(fnAssociations, await ytPlaceService.getFNAssociationTypes(), 'value', 'fnAssociationType', "text", "fnAssocationDesc");
-
-                    let relationships = {
-                        placeTypes: { data: placeTypes },
-                        fnNames: { data: fnNames },
-                        altNames: { data: altNames },
-                        histories: { data: histories },
-                        fnAssociations: { data: fnAssociations },
-                    };
-
-                    return res.send({
-                        data: place,
-                        relationships
-                    });
-                }
-                else {
-                    return res.status(404).send("Place not found");
-                }
-            })
-            .catch(err => {
-                console.error(err)
-                return res.status(404).send("Place not found");
-            })
-    });
-
-ytPlaceRouter.post("/",
-    [
-        body("place.name").isString().bail().notEmpty().trim()
-    ],
-    async (req: Request, res: Response) => {
-        const errors = validationResult(req);
-        const { place = {}, altNames = [], fnNames = [], fnAssociations = [], placeTypes = [] } = req.body;
-
-        if (!errors.isEmpty()) {
-            console.log('Validation error');
-            console.log(errors.array());
-            return res.status(400).json({ errors: errors.array() }); 
-        }
-
-        // YtPlaceService returning a YtPlace object was causing errors so using the same structure as boat-router
-        delete place.id;
-        const response = await db.insert(place)
-        .into('place.place')
-        .returning('*')
-        .then(async rows => {
-            const newPlace = rows[0]; 
-
-            if (altNames.length) {
-                const newAltNames = altNames.map((x: any) => ({ ...x, placeId: newPlace.Id }));     
-                for (let on of newAltNames) {
-                    delete on.id; 
-                    await ytPlaceService.addAlternateName(on); 
-                }
-            }
-
-            if (fnNames.length) {
-                const newFnNames = fnNames.map((x: any) => ({ ...x, placeId: newPlace.Id }));     
-                for (let on of newFnNames) {
-                    delete on.id; 
-                    await ytPlaceService.addFirstNationName(on);
-                }
-            }
-
-            if (fnAssociations.length) {
-                const newFnAssocations = fnAssociations.map((x: any) => ({ ...x, placeId: newPlace.Id }));     
-                for (let on of newFnAssocations) {
-                    await ytPlaceService.addFNAssociation(on);
-                }
-            }
-
-            if (placeTypes.length) {
-                const newPlaceTypes = placeTypes.map((x: any) => ({ placeTypeLookupId: x, placeId: newPlace.Id }));   
-                for (let on of newPlaceTypes) {
-                    await ytPlaceService.addPlaceType(on); 
-                }
-            }
-
-            return { id: newPlace.Id }; 
-
-        })
-        .catch(err => {
-            console.error("Database Error", err); 
-            return res.status(400).json({ errors: err });
-        });
-
-        return res.json({ messages: [{ variant: "success", text: "Place added" }], data: response });
-    });
-
-ytPlaceRouter.put("/:id",
-    [
-        param("id").isInt().notEmpty(),
-        body("name").isString().bail().notEmpty().trim()
-    ], ReturnValidationErrors,
-    async (req: Request, res: Response) => {
-        let { id } = req.params;
-        let { photos, placeTypes, fnNames, altNames, histories, fnAssociations } = req.body;
-        let updater = req.body;
-        delete updater.photos;
-        delete updater.placeTypes;
-        delete updater.fnNames;
-        delete updater.altNames;
-        delete updater.histories;
-        delete updater.fnAssociations;
-
-        await ytPlaceService.updatePlace(parseInt(id), updater);
-
-        // Update alternate names
-        let oldNames = await ytPlaceService.getAltNamesFor(parseInt(id));
-        altNames = altNames.map((n: AlternateName) => Object.assign(n, { alternateName: n.alternateName.trim() }));
-
-        for (let on of oldNames) {
->>>>>>> 68a9650 (Finished create/edit places form and print screen. Only the photos widget is left)
             let match = altNames.filter((n: AlternateName) => n.alternateName == on.alternateName);
             if (match.length == 0) {
                 await ytPlaceService.removeAlternateName(on.id);
@@ -497,9 +303,8 @@ ytPlaceRouter.put("/:id",
                 delete on.id;
                 await ytPlaceService.addAlternateName(on);
             }
-        }
+        }*/
 
-<<<<<<< HEAD
 		// Update place types - note that placeTypes is just an array of placeTypeLookupIds
 		let oldPlaceTypes = await ytPlaceService.getPlaceTypesFor(parseInt(id));
 
@@ -558,68 +363,19 @@ ytPlaceRouter.put("/:id",
 
 		// FnNames
 		/*let oldFnNames = await ytPlaceService.getFirstNationNamesFor(parseInt(id));
-=======
-        // Update place types - note that placeTypes is just an array of placeTypeLookupIds
-        let oldPlaceTypes = await ytPlaceService.getPlaceTypesFor(parseInt(id));
-
-        for (let on of oldPlaceTypes) {
-            let match = placeTypes.filter((n: Number) => n == on.placeTypeLookupId);
-            if (match.length == 0) {
-                let placeTypeInsert = new PlaceType ();
-                placeTypeInsert.placeId = parseInt(id);
-                placeTypeInsert.placeTypeLookupId = on.placeTypeLookupId;
-                await ytPlaceService.removePlaceType(placeTypeInsert);
-            }
-        }
-        for (let on of placeTypes) {
-            let match = oldPlaceTypes.filter((n: PlaceType) => n.placeTypeLookupId == on);
-            if (match.length == 0) {
-                let placeTypeInsert = new PlaceType ();
-                placeTypeInsert.placeId = parseInt(id);
-                placeTypeInsert.placeTypeLookupId = on;
-                await ytPlaceService.addPlaceType(placeTypeInsert);
-            }
-        }
-
-        // FnAssociations
-        let oldFnAssocations = await ytPlaceService.getFNAssociationsFor(parseInt(id));
-        for (let on of oldFnAssocations) {
-            let match = fnAssociations.filter((n: FnAssociation) => n.fnAssociationType == on.fnAssociationType && n.firstNationId == on.firstNationId);
-            if (match.length == 0) {
-                let FnAssociationInsert = new FnAssociation ();
-                FnAssociationInsert.placeId = parseInt(id);
-                FnAssociationInsert.fnAssociationType = on.fnAssociationType;
-                FnAssociationInsert.firstNationId = on.firstNationId;
-                await ytPlaceService.removeFNAssociation(FnAssociationInsert);
-            }
-        }
-        for (let on of fnAssociations) {
-            let match = oldFnAssocations.filter((n: FnAssociation) => n.fnAssociationType == on.fnAssociationType && n.firstNationId == on.firstNationId);
-            if (match.length == 0) {
-                on.placeId = parseInt(id);
-                delete on.fnAssocationDesc;
-                delete on.firstNationDescription;
-                await ytPlaceService.addFNAssociation(on);
-            }
-        }
-
-        // FnNames
-        let oldFnNames = await ytPlaceService.getFirstNationNamesFor(parseInt(id));
->>>>>>> 68a9650 (Finished create/edit places form and print screen. Only the photos widget is left)
         for (let on of oldFnNames) {
-            let match = fnNames.filter((n: FirstNationName) => n.fnName == on.fnName && n.fnDesription == on.fnDesription && n.fnLanguage == on.fnLanguage);
+            let match = fnNames.filter((n: FirstNationName) => n.fnName == on.fnName && n.fnDescription == on.fnDescription && n.fnLanguage == on.fnLanguage);
             if (match.length == 0) {
                 await ytPlaceService.removeFirstNationName(on.id);
             }
         }
         for (let on of fnNames) {
-            let match = oldFnNames.filter((n: FirstNationName) => n.fnName == on.fnName && n.fnDesription == on.fnDesription && n.fnLanguage == on.fnLanguage);
+            let match = oldFnNames.filter((n: FirstNationName) => n.fnName == on.fnName && n.fnDescription == on.fnDescription && n.fnLanguage == on.fnLanguage);
             if (match.length == 0) {
                 on.placeId = parseInt(id);
                 delete on.id;
                 await ytPlaceService.addFirstNationName(on);
             }
-<<<<<<< HEAD
         }*/
 
 		return res.json({
@@ -653,12 +409,17 @@ ytPlaceRouter.put("/:id",
 
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
-=======
->>>>>>> 68a9650 (Finished create/edit places form and print screen. Only the photos widget is left)
         }
 
-        return res.json({ messages: [{ variant: "success", text: "Place updated" }] });
+        const id = req.params.id as string;
+        let result = await ytPlaceService.updatePlace(parseInt(id), req.body as YtPlace).then(item => item)
+            .catch(err => {
+                return res.json({ errors: [err.originalError.info.message] });
+            });
+
+        return res.json({ data: result });
     });
+*/
 
 function combine(
 	list1: Array<any>,
--- src/web/src/components/Administration/AdminDashboard.vue
+++ src/web/src/components/Administration/AdminDashboard.vue
@@ -1,152 +1,127 @@
 <template>
-    <v-container>
-        <h1>Administration</h1>
-        <Breadcrumbs/>
-        <v-row>
-            <v-col
-                cols="6"
-                >
-                    <v-card
-                    elevation="1">
-                        <v-toolbar
-                            color="grey"
-                            dark
-                            flat
-                            >
+  <v-container>
+    <h1>Administration</h1>
+    <Breadcrumbs />
+    <v-row>
+      <v-col cols="6">
+        <v-card elevation="1">
+          <v-toolbar color="grey" dark flat>
+            <v-toolbar-title>Administration</v-toolbar-title>
 
-                            <v-toolbar-title>Administration</v-toolbar-title>
+            <v-spacer></v-spacer>
+          </v-toolbar>
+          <v-list class="pb-0 pt-0">
+            <v-list-item-group v-model="usermodel">
+              <v-list-item @click="goTo('/admin/users')">
+                <v-list-item-icon>
+                  <v-icon v-text="'mdi-account-group'"></v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>User Management</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-card>
+      </v-col>
+      <v-col cols="6">
+        <v-card elevation="1">
+          <v-toolbar color="grey" dark flat>
+            <v-toolbar-title>Look-up Table Managment</v-toolbar-title>
 
-                            <v-spacer></v-spacer>
-
-                        </v-toolbar>
-                        <v-list class="pb-0 pt-0">
-                            <v-list-item-group v-model="usermodel">
-                                <v-list-item @click="goTo('/admin/users')">
-                                <v-list-item-icon>
-                                    <v-icon v-text="'mdi-account-group'"></v-icon>
-                                </v-list-item-icon>
-                                <v-list-item-content>
-                                    <v-list-item-title >User Management</v-list-item-title>
-                                </v-list-item-content>
-                                </v-list-item>
-                            </v-list-item-group>
-                        </v-list>
-                    </v-card>
-                </v-col>
-             <v-col
-                cols="6"
-                >
-                    <v-card
-                    elevation="1">
-                         <v-toolbar
-                            color="grey"
-                            dark
-                            flat
-                            >
-
-                            <v-toolbar-title>Look-up Table Managment</v-toolbar-title>
-
-                            <v-spacer></v-spacer>
-
-                        </v-toolbar>
-                        <v-list class="pb-0 pt-0">
-                        <v-list-item-group v-model="tablemodel" >
-                            <v-list-item
-                            v-for="(item, i) in items"
-                            :key="i"
-                            @click="goTo(item.url)"
-                            >
-                            <v-list-item-icon>
-                                <v-icon v-text="item.icon"></v-icon>
-                            </v-list-item-icon>
-                            <v-list-item-content>
-                                <v-list-item-title v-text="item.text"></v-list-item-title>
-                            </v-list-item-content>
-                            </v-list-item>
-                        </v-list-item-group>
-                        </v-list>
-                    </v-card>
-                </v-col>
-        </v-row>
-        
-    </v-container>
+            <v-spacer></v-spacer>
+          </v-toolbar>
+          <v-list class="pb-0 pt-0">
+            <v-list-item-group v-model="tablemodel">
+              <v-list-item
+                v-for="(item, i) in items"
+                :key="i"
+                @click="goTo(item.url)"
+              >
+                <v-list-item-icon>
+                  <v-icon v-text="item.icon"></v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.text"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 import Breadcrumbs from "../Breadcrumbs";
 export default {
-    components: {
-        Breadcrumbs
+  components: {
+    Breadcrumbs,
+  },
+  data: () => ({
+    icon: null,
+    usermodel: null,
+    tablemodel: null,
+    items: [
+      {
+        icon: "mdi-ferry",
+        text: "Boats - Vessel Type",
+        url: "/admin/vessel_type",
+      },
+      {
+        icon: "mdi-wrench",
+        text: "Burials - Occupations",
+        url: "/admin/burials/occupations",
+      },
+      {
+        icon: "mdi-wallet-membership",
+        text: "Burials - Memberships",
+        url: "/admin/burials/memberships",
+      },
+      {
+        icon: "mdi-database",
+        text: "Burials - Cementeries",
+        url: "/admin/burials/cementeries",
+      },
+      {
+        icon: "mdi-shape",
+        text: "Burials - Religions",
+        url: "/admin/burials/religions",
+      },
+      {
+        icon: "mdi-bug",
+        text: "Burials - Causes",
+        url: "/admin/burials/causes",
+      },
+      {
+        icon: "mdi-camera",
+        text: "Photos - Owner",
+        url: "",
+      },
+      {
+        icon: "mdi-map-marker-multiple",
+        text: "Community",
+        url: "",
+      },
+      {
+        icon: "mdi-run",
+        text: "Contributing Resources",
+        url: "",
+      },
+      {
+        icon: "mdi-home",
+        text: "Place Type",
+        url: "/admin/placetype",
+      },
+    ],
+    model: 1,
+  }),
+  methods: {
+    goTo(url) {
+      if (url == "") return;
+      this.$router.push(url);
     },
-    data: () =>({
-        icon: null,
-        usermodel: null,
-        tablemodel: null,
-        items: [
-        {
-          icon: 'mdi-ferry',
-          text: 'Boats - Vessel Type',
-          url: "/admin/vessel_type"
-        },
-        {
-          icon: 'mdi-wrench',
-          text: 'Burials - Occupations',
-          url: "/admin/burials/occupations"
-        },
-        {
-          icon: 'mdi-wallet-membership',
-          text: 'Burials - Memberships',
-          url: "/admin/burials/memberships"
-        },
-        {
-          icon: 'mdi-database',
-          text: 'Burials - Cementeries',
-          url: "/admin/burials/cementeries"
-        },
-        {
-          icon: 'mdi-shape',
-          text: 'Burials - Religions',
-          url: "/admin/burials/religions"
-        },
-        {
-          icon: 'mdi-bug',
-          text: 'Burials - Causes',
-          url: "/admin/burials/causes"
-        },
-        {
-          icon: 'mdi-camera',
-          text: 'Photos - Owner',
-          url: ""
-        },
-        {
-          icon: 'mdi-map-marker-multiple',
-          text: 'Community',
-          url: ""
-        },
-        {
-          icon: 'mdi-run',
-          text: 'Contributing Resources',
-          url: ""
-        },
-        {
-<<<<<<< HEAD
-          icon: 'mdi-map',
-=======
-          icon: 'mdi-home',
->>>>>>> 6472a9d (Built most of places form (except map and photos widgets))
-          text: 'Place Type',
-          url: "/admin/placetype"
-        },
-        
-      ],
-      model: 1,
-    }),
-    methods:{
-        goTo(url){
-            if(url == "")
-                return;
-            this.$router.push(url);
-        }
-    }
-}
+  },
+};
 </script>
