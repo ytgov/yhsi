3568770c5cb718e769998f15e26bfece62377896
Angel Gomez <angel@bizont.ca>
2022-03-07 11:17:30 -0700

    Burials view and grid first steps (more testing needed)

--- src/api/middleware/index.ts
+++ src/api/middleware/index.ts
@@ -9,11 +9,11 @@ export function RequiresAuthentication(
 ) {
 	// DJPRATT temporary hack to get around login issue
 	 return next();
-	/*
+	
 	if (req.oidc.isAuthenticated()) {
 		return next();
 	}
-*/
+
 	res.status(401).send('Not authenticated'); //;.redirect('/api/auth/login');
 }
 
