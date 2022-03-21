19bd3c26108b0a8e5dacfcc8de7864cd59729409
Marlen Brunner <klondikemarlen@gmail.com>
2022-03-21 14:13:13 -0700

    :recycle: push generic sort builder function to common location.

--- src/api/services/index.ts
+++ src/api/services/index.ts
@@ -10,6 +10,7 @@ export * from "./aircrash-service";
 export * from "./photo-batch-service";
 export * from "./boat-owner-service";
 export * from "./people-service";
+
 export interface QueryStatement {
 	field: string;
 	operator: string;
@@ -24,4 +25,16 @@ export interface SortStatement {
 export enum SortDirection {
 	ASCENDING = 'asc',
 	DESCENDING = 'desc',
+}
+
+export function buildDatabaseSort(
+	sortBy: Array<string>,
+	sortDesc: Array<boolean>
+): Array<SortStatement> {
+	return sortBy.map((field: string, index: number) => ({
+		field,
+		direction: sortDesc[index]
+			? SortDirection.ASCENDING
+			: SortDirection.DESCENDING,
+	}));
 }
