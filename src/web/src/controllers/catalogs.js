56fd76090d150cfb0663717f24822b4653ae9096
Angel Gomez <angel@bizont.ca>
2022-03-21 13:30:35 -0700

    Added the api calls for the burial catalogs

--- src/web/src/controllers/catalogs.js
+++ src/web/src/controllers/catalogs.js
@@ -55,60 +55,6 @@ export default {
         console.error(error);
       });
   },
-  async getCauses() {
-    return await api.get(`catalogs/cause`)
-      .then(res => {
-        return res.data;
-      }).catch(error => {
-        // handle error
-        console.error(error);
-      });
-  },
-  async getReligions() {
-    return await api.get(`catalogs/religion`)
-      .then(res => {
-        return res.data;
-      }).catch(error => {
-        // handle error
-        console.error(error);
-      });
-  },
-  async getCemetaries() {
-    return await api.get(`catalogs/cemetary`)
-      .then(res => {
-        return res.data;
-      }).catch(error => {
-        // handle error
-        console.error(error);
-      });
-  },
-  async getOccupations() {
-    return await api.get(`catalogs/occupation`)
-      .then(res => {
-        return res.data;
-      }).catch(error => {
-        // handle error
-        console.error(error);
-      });
-  },
-  async getMemberships() {
-    return await api.get(`catalogs/membership`)
-      .then(res => {
-        return res.data;
-      }).catch(error => {
-        // handle error
-        console.error(error);
-      });
-  },
-  async getRelationships() {
-    return await api.get(`catalogs/relationship`)
-      .then(res => {
-        return res.data;
-      }).catch(error => {
-        // handle error
-        console.error(error);
-      });
-  },
   async getOriginalMedia() {
     return await api.get(`catalogs/originalmedia`)
       .then(res => {
@@ -190,6 +136,175 @@ export default {
         // handle error
         console.error(error);
       });
-  }
+  },
+//CAUSES
+  async getCauses() {
+    return await api.get(`catalogs/cause`)
+      .then(res => {
+        return res.data;
+      }).catch(error => {
+        // handle error
+        console.error(error);
+      });
+  },
+  async putCauses(id, data) {
+    return await api.put(`catalogs/cause/${id}`, data)
+      .then(res => {
+        return res.data;
+      }).catch(error => {
+        // handle error
+        console.error(error);
+      });
+  },
+  async postCauses(id, data) {
+    return await api.post(`catalogs/cause/${id}`, data)
+      .then(res => {
+        return res.data;
+      }).catch(error => {
+        // handle error
+        console.error(error);
+      });
+  },
+  //RELIGIONS
+  async getReligions() {
+    return await api.get(`catalogs/religion`)
+      .then(res => {
+        return res.data;
+      }).catch(error => {
+        // handle error
+        console.error(error);
+      });
+  },
+  async putReligion(id, data) {
+    return await api.put(`catalogs/religion/${id}`, data)
+      .then(res => {
+        return res.data;
+      }).catch(error => {
+        // handle error
+        console.error(error);
+      });
+  },
+  async postCemetary(id, data) {
+    return await api.post(`catalogs/religion/${id}`, data)
+      .then(res => {
+        return res.data;
+      }).catch(error => {
+        // handle error
+        console.error(error);
+      });
+  },
+  //CEMETARIES
+  async getCemetaries() {
+    return await api.get(`catalogs/cemetary`)
+      .then(res => {
+        return res.data;
+      }).catch(error => {
+        // handle error
+        console.error(error);
+      });
+  },
+  async putCemetary(id, data) {
+    return await api.put(`catalogs/cemetary/${id}`, data)
+      .then(res => {
+        return res.data;
+      }).catch(error => {
+        // handle error
+        console.error(error);
+      });
+  },
+  async postCemetary(id, data) {
+    return await api.post(`catalogs/cemetary/${id}`, data)
+      .then(res => {
+        return res.data;
+      }).catch(error => {
+        // handle error
+        console.error(error);
+      });
+  },
+  //OCCUPATIONS
+  async getOccupations() {
+    return await api.get(`catalogs/occupation`)
+      .then(res => {
+        return res.data;
+      }).catch(error => {
+        // handle error
+        console.error(error);
+      });
+  },
+  async putOccupation(id, data) {
+    return await api.put(`catalogs/occupation/${id}`, data)
+      .then(res => {
+        return res.data;
+      }).catch(error => {
+        // handle error
+        console.error(error);
+      });
+  },
+  async postOccupation(id, data) {
+    return await api.post(`catalogs/occupation/${id}`, data)
+      .then(res => {
+        return res.data;
+      }).catch(error => {
+        // handle error
+        console.error(error);
+      });
+  },
+//MEMBERSHIPS
+  async getMemberships() {
+    return await api.get(`catalogs/membership`)
+      .then(res => {
+        return res.data;
+      }).catch(error => {
+        // handle error
+        console.error(error);
+      });
+  },
+  async putMembership(id, data) {
+    return await api.put(`catalogs/membership/${id}`, data)
+      .then(res => {
+        return res.data;
+      }).catch(error => {
+        // handle error
+        console.error(error);
+      });
+  },
+  async postMembership(id, data) {
+    return await api.post(`catalogs/membership/${id}`, data)
+      .then(res => {
+        return res.data;
+      }).catch(error => {
+        // handle error
+        console.error(error);
+      });
+  },
+  //RELATIONSHIPS
+  async getRelationships() {
+    return await api.get(`catalogs/relationship`)
+      .then(res => {
+        return res.data;
+      }).catch(error => {
+        // handle error
+        console.error(error);
+      });
+  },
+  async putRelationship(id, data) {
+    return await api.put(`catalogs/relationship/${id}`, data)
+      .then(res => {
+        return res.data;
+      }).catch(error => {
+        // handle error
+        console.error(error);
+      });
+  },
+  async postRelationship(id, data) {
+    return await api.post(`catalogs/relationship/${id}`, data)
+      .then(res => {
+        return res.data;
+      }).catch(error => {
+        // handle error
+        console.error(error);
+      });
+  },
+  
 
 } 
