66705e98caa09156e43c195275f109a15e7e1811
Michael Johnson <michael@icefoganalytics.com>
2022-03-25 16:25:59 -0700

    Backend components for security

---
+++ src/api/middleware/authorization.ts
@@ -0,0 +1,58 @@
+import { Request, Response, NextFunction } from "express";
+import { User } from "../models";
+
+const USER_ACTIVE_STATUS = "Active";
+
+export const UserRoles = {
+    ADMINISTRATOR: "Administrator",
+    SITE_ADMIN: "Site Admin",
+    SITE_EDITOR: "Site Editor",
+    SITE_VIEWER: "Site Viewer",
+    PHOTO_ADMIN: "Photo Admin",
+    PHOTO_EDITOR: "Photo Editor",
+    PHOTO_VIEWER: "Photo Viewer",
+    SITE_VIEWER_LIMITED: "Site Viewer Limited",
+    AIRPLANE_CRASH_EDITOR: "Airplane Crash Editor",
+    BOATS_EDITOR: "Boats Editor",
+    PLACE_EDITOR: "Places Editor",
+    BURIALS_EDITOR: "Burials Editor",
+    PEOPLE_EDITOR: "People Editor",
+    YRHP_EDITOR: "YRHP Editor"
+};
+
+export const UserRoleOptions = [
+    UserRoles.ADMINISTRATOR,
+    UserRoles.SITE_ADMIN,
+    UserRoles.SITE_EDITOR,
+    UserRoles.SITE_VIEWER,
+    UserRoles.SITE_VIEWER_LIMITED,
+    UserRoles.PHOTO_ADMIN,
+    UserRoles.PHOTO_EDITOR,
+    UserRoles.PHOTO_VIEWER,
+    UserRoles.AIRPLANE_CRASH_EDITOR,
+    UserRoles.BOATS_EDITOR,
+    UserRoles.PLACE_EDITOR,
+    UserRoles.BURIALS_EDITOR,
+    UserRoles.PEOPLE_EDITOR,
+    UserRoles.YRHP_EDITOR
+]
+
+export function authorize(roles: string[] = []) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        let currentUser = req.user as User;
+
+        if (currentUser.status != USER_ACTIVE_STATUS)
+            return res.status(401).json({ message: "Unauthorized - User inactive" });
+
+        // if route only requires an active user
+        if (roles.length == 0)
+            return next();
+
+        for (let role of roles) {
+            if (currentUser.roles && currentUser.roles.indexOf(role) >= 0)
+                return next();
+        }
+
+        return res.status(401).json({ message: 'Unauthorized - Missing role(s): ' + roles });
+    };
+}
