2a0f822df14f2256332bd81d7aa632145f291d80
Michael Johnson <michael@icefoganalytics.com>
2022-03-25 16:26:36 -0700

    Front-end changes for security and real users

---
+++ src/web/src/authorization.js
@@ -0,0 +1,17 @@
+
+export const UserRoles = {
+    ADMINISTRATOR: "Administrator",
+    SITE_ADMIN: "Site Admin",
+    SITE_EDITOR: "Site Editor",
+    SITE_VIEWER: "Site Viewer",
+    PHOTO_ADMIN: "Photo Admin",
+    PHOTO_EDITOR: "Photo Editor",
+    PHOTO_VIEWER: "Photo Viewer",
+    SITE_VIEWER_LIMITED: "Site Viewer Limited",
+    AIRPLANE_CRASH_EDITOR: "Airplane Crash Editor",
+    BOATS_EDITOR: "Boats Editor",
+    PLACE_EDITOR: "Places Editor",
+    BURIALS_EDITOR: "Burials Editor",
+    PEOPLE_EDITOR: "People Editor",
+    YRHP_EDITOR: "YRHP Editor"
+};
--- src/web/src/components/Administration/UserManagement/Grid/index.vue
+++ src/web/src/components/Administration/UserManagement/Grid/index.vue
@@ -1,123 +1,140 @@
 <template>
-  <div class="">
-    <!-- <v-btn color="secondary" class="float-right mb-0 mt-2 pl-2" to="/admin" exact style="height: auto; font-size: .8rem; padding: 6px 10px;"
+	<div class="">
+		<!-- <v-btn color="secondary" class="float-right mb-0 mt-2 pl-2" to="/admin" exact style="height: auto; font-size: .8rem; padding: 6px 10px;"
       ><v-icon class="mr-2" small>mdi-arrow-left</v-icon> Back to Administration</v-btn
     > -->
-    <v-breadcrumbs
-      :items="[
-        { text: 'Adminstration', to: '/admin', exact: true },
-        { text: 'User Management' },
-      ]"
-    ></v-breadcrumbs>
+		<v-breadcrumbs
+			:items="[
+				{ text: 'Adminstration', to: '/admin', exact: true },
+				{ text: 'User Management' },
+			]"
+		></v-breadcrumbs>
 
-    <h1>User Management</h1>
+		<h1>User Management</h1>
 
-    <div class="mt-2">
-      <v-card class="default px-3 py-3">
-        <v-card-text>
-          <v-row>
-            <v-col cols="8" class="d-flex">
-              <v-text-field
-                prepend-inner-icon="mdi-magnify"
-                background-color="white"
-                outlined
-                dense
-                label="Search"
-                v-model="search"
-                hide-details
-              ></v-text-field>
-            </v-col>
-            <v-col cols="4" class="d-flex">
-              <v-select
-                small-chips
-                multiple
-                :items="filterOptions"
-                v-model="selectedFilter"
-                label="Status filter"
-                dense
-                outlined
-                background-color="white"
-                hide-details
-              ></v-select>
-            </v-col>
-            <v-spacer></v-spacer>
-            <v-col cols="auto"> </v-col>
-          </v-row>
+		<div class="mt-2">
+			<v-card class="default px-3 py-3">
+				<v-card-text>
+					<v-row>
+						<v-col cols="8" class="d-flex">
+							<v-text-field
+								prepend-inner-icon="mdi-magnify"
+								background-color="white"
+								outlined
+								dense
+								label="Search"
+								v-model="search"
+								hide-details
+							></v-text-field>
+						</v-col>
+						<v-col cols="4" class="d-flex">
+							<v-select
+								small-chips
+								multiple
+								:items="filterOptions"
+								v-model="selectedFilter"
+								label="Status filter"
+								dense
+								outlined
+								background-color="white"
+								hide-details
+							></v-select>
+						</v-col>
+						<v-spacer></v-spacer>
+						<v-col cols="auto"> </v-col>
+					</v-row>
 
-          <v-data-table
-            :items="filteredData"
-            :headers="headers"
-            :loading="loading"
-            :search="search"
-            @click:row="handleClick"
-            :footer-props="{ 'items-per-page-options': [10, 30, 100] }"
-            class="clickable-row"
-          >
-          </v-data-table
-        ></v-card-text>
-      </v-card>
-    </div>
-  </div>
+					<v-data-table
+						:items="filteredData"
+						:headers="headers"
+						:loading="loading"
+						:search="search"
+						@click:row="handleClick"
+						:footer-props="{ 'items-per-page-options': [10, 30, 100] }"
+						class="clickable-row"
+					>
+					</v-data-table
+				></v-card-text>
+			</v-card>
+		</div>
+		<notifications ref="notifier"></notifications>
+	</div>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapActions } from 'vuex';
+import Notifications from '../../../Notifications.vue';
 export default {
-  name: "usersgrid",
-  data: () => ({
-    loading: false,
-    users: [],
-    search: "",
-    options: {},
-    totalLength: 0,
-    headers: [
-      { text: "Email", value: "Email" },
-      { text: "First Name", value: "FirstName" },
-      { text: "Last Name", value: "LastName" },
-      { text: "Status", value: "Status" },
-      { text: "Last Login", value: "LastLogin" },
-      //      { text: "Actions", value: "actions"}
-    ],
-    page: 1,
-    pageCount: 0,
-    iteamsPerPage: 10,
-    selectedFilter: ["Active"],
-    filterOptions: ["Active", "Expired", "Inactive"],
-  }),
-  async mounted() {
-    //this.getDataFromApi();
-    this.users = await this.loadUsers();
-  },
-  methods: {
-    ...mapActions("users", ["loadUsers"]),
+	components: { Notifications },
+	name: 'usersgrid',
+	data: () => ({
+		loading: false,
+		users: [],
+		search: '',
+		options: {},
+		totalLength: 0,
+		headers: [
+			{ text: 'Email', value: 'email' },
+			{ text: 'First Name', value: 'first_name' },
+			{ text: 'Last Name', value: 'last_name' },
+			{ text: 'Status', value: 'status' },
+			{ text: 'Last Login', value: 'last_login_date_display' },
+		],
+		page: 1,
+		pageCount: 0,
+		iteamsPerPage: 10,
+		selectedFilter: ['Active'],
+		filterOptions: ['Active', 'Expired', 'Inactive', 'Pending'],
+	}),
+	async mounted() {
+		this.loading = true;
+		this.loadUsers()
+			.then((resp) => {
+				if (resp.error) {
+					console.log('Response Error:', resp.error.message);
+					this.$router.push('/admin');
+				} else {
+					this.users = resp.data;
+				}
+			})
+			.catch((err) => {
+				console.log('ERROR', err);
+				this.users = [];
+			})
+			.finally(() => {
+				this.loading = false;
+			});
+	},
+	methods: {
+		...mapActions('users', ['loadUsers']),
 
-    handleClick(value) {
-      //Redirects the user to the edit user form
-      this.$router.push(`/admin/users/${value.UserId}`);
-    },
-  },
-  computed: {
-    filteredData() {
-      if (this.selectedFilter.length == 0) return this.users;
+		handleClick(value) {
+			//Redirects the user to the edit user form
+			this.$router.push(`/admin/users/${value.id}`);
+		},
+	},
+	computed: {
+		filteredData() {
+			if (this.selectedFilter.length == 0) return this.users;
 
-      let data = [];
+			let data = [];
 
-      for (let usr of this.users) {
-        if (this.selectedFilter.indexOf(usr.Status) >= 0) {
-          data.push(usr);
-        }
-      }
+			for (let usr of this.users) {
+				if (this.selectedFilter.indexOf(usr.status) >= 0) {
+					data.push(usr);
+				}
+			}
 
-      return data;
-    },
-  },
+			return data;
+		},
+	},
 };
 </script>
 
 <style scoped>
 .hoverclicklink:hover {
-  color: #0097a9;
-  text-decoration: underline;
-  cursor: pointer;
+	color: #0097a9;
+	text-decoration: underline;
+	cursor: pointer;
 }
 </style>
