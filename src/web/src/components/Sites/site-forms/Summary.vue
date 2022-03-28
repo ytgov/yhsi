5ba7d99e97c4693f520481c3932753dbdfac6652
Marlen Brunner <klondikemarlen@gmail.com>
2022-03-28 16:30:14 -0700

    :art: Basic linting/prettification of site-forms/Summary.vue.

--- src/web/src/components/Sites/site-forms/Summary.vue
+++ src/web/src/components/Sites/site-forms/Summary.vue
@@ -2,187 +2,207 @@
   <div>
     <h2 class="mt-2 mb-0 ml-4 d-flex justify-space-between">
       <span class="mt-2">Summary</span>
-      <v-btn class="my-0" color="primary" @click="saveChanges">Save</v-btn>
+      <v-btn
+        class="my-0"
+        color="primary"
+        @click="saveChanges"
+      >
+        Save
+      </v-btn>
     </h2>
-    <v-divider class="mb-5"></v-divider>
+    <v-divider class="mb-5" />
     <v-form v-model="valid">
       <v-container>
         <v-row>
           <v-col cols="6">
             <v-text-field
+              v-model="fields.yHSIId"
               dense
               outlined
-              v-model="fields.yHSIId"
               label="YHSI ID"
               required
               readonly
               append-icon="mdi-lock"
-            ></v-text-field>
+            />
 
             <v-select
+              v-model="fields.designations"
               dense
               outlined
-              v-model="fields.designations"
               :items="designationOptions"
               clearable
               label="Designations"
-            ></v-select>
+            />
 
             <v-select
+              v-model="fields.category"
               dense
               outlined
-              v-model="fields.category"
               clearable
               label="CRHP category"
               :items="categoryOptions"
               item-text="text"
               item-value="id"
-            ></v-select>
-
-          <v-select
-            dense
-            outlined
-            v-model="fields.records"
-            :items="recordOptions"
-            item-text="text"
-            item-value="value"
-            clearable
-            label="Records"
-          ></v-select>
+            />
 
             <v-select
+              v-model="fields.records"
               dense
               outlined
+              :items="recordOptions"
+              item-text="text"
+              item-value="value"
+              clearable
+              label="Records"
+            />
+
+            <v-select
               v-model="fields.records"
+              dense
+              outlined
               :items="recordOptions"
               clearable
               label="Records"
-            ></v-select>
+            />
 
             <v-checkbox
+              v-model="fields.showInRegister"
               dense
               outlined
-              v-model="fields.showInRegister"
               label="Show in Register?"
-            ></v-checkbox>
+            />
           </v-col>
           <v-col cols="6">
-          <v-text-field
-            dense
-            outlined
-            v-model="fields.primaryName"
-            label="Primary name"
-            required
-          ></v-text-field>
-           
+            <v-text-field
+              v-model="fields.primaryName"
+              dense
+              outlined
+              label="Primary name"
+              required
+            />
 
-           <v-card class="default mb-5">
-            <v-card-text>
-              <h3>Secondary Names</h3>
-              <v-row v-for="(item, i) of names" :key="i">
-                <v-col cols="10">
-                  <v-text-field
-                    dense
-                    outlined
-                    background-color="white"
-                    v-model="item.description"
-                    label="Secondary name"
-                    hide-details
-                  >
-                  </v-text-field>
-                </v-col>
-                <v-col cols="2">
-                  <v-btn
-                    color="warning"
-                    x-small
-                    fab
-                    title="Remove"
-                    class="my-0 float-right"
-                    @click="removeName(i)"
-                    ><v-icon>mdi-close</v-icon></v-btn
-                  >
-                </v-col>
-              </v-row>    
-                <v-btn
-                class="mt-5" color="info"
-                @click="addName()"
+            <v-card class="default mb-5">
+              <v-card-text>
+                <h3>Secondary Names</h3>
+                <v-row
+                  v-for="(item, i) of names"
+                  :key="i"
                 >
-                    Add New Secondary Name
+                  <v-col cols="10">
+                    <v-text-field
+                      v-model="item.description"
+                      dense
+                      outlined
+                      background-color="white"
+                      label="Secondary name"
+                      hide-details
+                    />
+                  </v-col>
+                  <v-col cols="2">
+                    <v-btn
+                      color="warning"
+                      x-small
+                      fab
+                      title="Remove"
+                      class="my-0 float-right"
+                      @click="removeName(i)"
+                    >
+                      <v-icon>mdi-close</v-icon>
+                    </v-btn>
+                  </v-col>
+                </v-row>
+                <v-btn
+                  class="mt-5"
+                  color="info"
+                  @click="addName()"
+                >
+                  Add New Secondary Name
                 </v-btn>
               </v-card-text>
             </v-card>
-               
-                  <v-text-field
-                    dense
-                    outlined
-                    v-model="fields.contributingResources"
-                    label="Contribuiting resources"
-                    required
-                  ></v-text-field>
 
-                  <v-card class="default mb-5">
-                    <v-card-text>
-                      <h3>Historical Patterns</h3>
-                      <div class="row" v-for="(item, i) of historicalPatterns" :key="i">
-                        <div class="col-md-10">
-                          <v-select
-                            dense
-                            outlined
-                            :items="historicalPatternOptions"
-                            v-model="item.historicalPatternType"
-                            item-text="text"
-                            item-value="value"
-                            background-color="white"
-                            label="Historical pattern"
-                          ></v-select>
-                          <v-text-field
-                            dense
-                            outlined
-                            v-model="item.comments"
-                            background-color="white"
-                            label="Comments"
-                            required
-                            hide-details
-                          ></v-text-field>
-                        </div>
+            <v-text-field
+              v-model="fields.contributingResources"
+              dense
+              outlined
+              label="Contribuiting resources"
+              required
+            />
 
-                        <div class="col-md-2">
-                          <v-btn
-                            color="warning"
-                            x-small
-                            fab
-                            title="Remove"
-                            class="my-0 float-right"
-                            @click="removePattern(i)"
-                            ><v-icon>mdi-close</v-icon>
-                          </v-btn>
-                        </div>
+            <v-card class="default mb-5">
+              <v-card-text>
+                <h3>Historical Patterns</h3>
+                <div
+                  v-for="(item, i) of historicalPatterns"
+                  :key="i"
+                  class="row"
+                >
+                  <div class="col-md-10">
+                    <v-select
+                      v-model="item.historicalPatternType"
+                      dense
+                      outlined
+                      :items="historicalPatternOptions"
+                      item-text="text"
+                      item-value="value"
+                      background-color="white"
+                      label="Historical pattern"
+                    />
+                    <v-text-field
+                      v-model="item.comments"
+                      dense
+                      outlined
+                      background-color="white"
+                      label="Comments"
+                      required
+                      hide-details
+                    />
+                  </div>
 
-                        <div v-if="i < historicalPatterns.length - 1" class="col-md-12">
-                          <hr />
-                        </div>
-                      </div>
-                      <v-btn class="mt-5" color="info" @click="addPattern()">
-                        Add Historical Pattern
-                      </v-btn>
-                    </v-card-text>
-                  </v-card>
+                  <div class="col-md-2">
+                    <v-btn
+                      color="warning"
+                      x-small
+                      fab
+                      title="Remove"
+                      class="my-0 float-right"
+                      @click="removePattern(i)"
+                    >
+                      <v-icon>mdi-close</v-icon>
+                    </v-btn>
+                  </div>
+
+                  <div
+                    v-if="i < historicalPatterns.length - 1"
+                    class="col-md-12"
+                  >
+                    <hr />
+                  </div>
+                </div>
+                <v-btn
+                  class="mt-5"
+                  color="info"
+                  @click="addPattern()"
+                >
+                  Add Historical Pattern
+                </v-btn>
+              </v-card-text>
+            </v-card>
           </v-col>
-          
         </v-row>
       </v-container>
     </v-form>
-  </div> 
+  </div>
 </template>
 
 <script>
-import axios from "axios";
-import store from "../../../store";
-import { PLACE_URL, STATIC_URL } from "../../../urls";
+import axios from 'axios';
+import store from '@/store';
+import { PLACE_URL, STATIC_URL } from '@/urls';
 
 /* Important**, field data that was not found on the swaggerhub api docs provided was assumed to be in development, hence, some placeholder variables were created. */
 export default {
-  name: "Summary",
+  name: 'Summary',
   data: () => ({
     valid: false,
     loadedId: 0,
@@ -192,32 +212,32 @@ export default {
     siteCategoryOptions: [],
     recordOptions: [],
     categoryOptions: [
-      { text: "None Selected", id: 0 },
-      { text: "Building", id: 1 },
-      { text: "District", id: 2 },
-      { text: "Place", id: 3 },
-      { text: "Structure", id: 4 },
+      { text: 'None Selected', id: 0 },
+      { text: 'Building', id: 1 },
+      { text: 'District', id: 2 },
+      { text: 'Place', id: 3 },
+      { text: 'Structure', id: 4 },
     ],
     historicalPatternOptions: [],
 
     generalRules: [
-      (v) => !!v || "This input is required",
+      (v) => !!v || 'This input is required',
       (v) =>
-        (v && v.length <= 20) || "This input must be less than 20 characters",
+        (v && v.length <= 20) || 'This input must be less than 20 characters',
     ],
     names: [],
     historicalPatterns: [],
     fields: {
-      primaryName: "", //
-      yHSIId: "", //
+      primaryName: '', //
+      yHSIId: '', //
       /* Placeholder variables below this line **Read above** */
       secondaryNames: [],
       /*Field data from the swaggerhub api docs below this line*/
-      contributingResources: "", //
-      category: "", //
-      designations: "", //
+      contributingResources: '', //
+      category: '', //
+      designations: '', //
       records: [], //
-      showInRegister: "", //
+      showInRegister: '', //
       siteCategories: [], //
     },
   }),
@@ -242,7 +262,7 @@ export default {
     });
   },
   watch: {
-    "$route.params.id": {
+    '$route.params.id': {
       handler: function (id) {
         this.entity_id = id;
         this.loadItem(this.entity_id);
@@ -270,11 +290,11 @@ export default {
       this.fields.designations = parseInt(place.data.designations);
       this.fields.records = parseInt(place.data.records);
 
-      store.dispatch("addSiteHistory", place.data);
+      store.dispatch('addSiteHistory', place.data);
       this.$parent.siteName = this.fields.primaryName;
     },
     addName() {
-      this.names.push({ description: "", placeId: this.entity_id });
+      this.names.push({ description: '', placeId: this.entity_id });
     },
     removeName(index) {
       this.names.splice(index, 1);
@@ -282,7 +302,7 @@ export default {
     addPattern() {
       this.historicalPatterns.push({
         historicalPatternType: 1,
-        comments: "",
+        comments: '',
         placeId: this.entity_id,
       });
     },
@@ -307,16 +327,16 @@ export default {
         .put(`${PLACE_URL}/${this.loadedId}/summary`, body)
         .then((resp) => {
           //this.setPlace(resp.data);
-          this.$emit("showAPIMessages", resp.data);
+          this.$emit('showAPIMessages', resp.data);
         })
         .catch((err) => {
-          this.$emit("showError", err);
+          this.$emit('showError', err);
         });
     },
-    removeItem(objName, position){
-        if (position > -1) {
-          this.fields[objName].splice(position, 1);
-        }
+    removeItem(objName, position) {
+      if (position > -1) {
+        this.fields[objName].splice(position, 1);
+      }
     },
   },
 };
