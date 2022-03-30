ddb8095d9d1b5b3d0dbd3cfbfa5fbd690fda9720
Marlen Brunner <klondikemarlen@gmail.com>
2022-03-30 10:55:28 -0700

    :sparkles: Basic wiring of SiteChangeRequest component.

---
+++ src/web/src/components/sites-change-requests/SiteChangeRequest.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+v-card
+	v-card-title
+		| Site Change Request
+	v-card-text
+		| Site Change Request {{ siteId }}
+</template>
+
+<script>
+export default {
+	name: 'SiteChangeRequest',
+	components: {},
+	props: {
+		siteId: {
+			type: [Number, String],
+			required: true,
+		},
+	},
+	data: () => ({}),
+	computed: {},
+	watch: {},
+	mounted() {},
+	methods: {},
+};
+</script>
+
+<style scoped></style>
