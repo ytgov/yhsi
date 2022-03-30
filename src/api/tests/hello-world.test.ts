dd07f040a8d88107e56a17c41c9c5fb439c28f33
Marlen Brunner <klondikemarlen@gmail.com>
2022-03-30 09:40:12 -0700

    :wrench: Basic test suite for api files.
    Needs automatic require of chai methods, dockerization, and database isolation.

---
+++ src/api/tests/hello-world.test.ts
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+
+describe('Hello World', () => {
+  it('boots the test suite correctly', () => {
+    expect(true).to.be.true;
+  });
+
+  it('watches correctly', () => {
+    expect(false).to.be.false;
+  });
+});
