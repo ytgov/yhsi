4e99e526a90c569dcf0aa749884347863ee26ca3
Marlen Brunner <klondikemarlen@gmail.com>
2022-04-05 22:43:00 -0700

    :beetle: Avoid sending invalid columns to place edit table.
    Also don't send and id column as that breaks inserts.

--- src/web/src/store/places.js
+++ src/web/src/store/places.js
@@ -2,54 +2,52 @@ import { isEmpty } from 'lodash';
 
 import api from '@/apis/places-api';
 
-const state = {
-  place: {},
-  loading: false,
-};
-
-const getters = {
-  hasPendingChanges: (state) => state.place.hasPendingChanges,
-  historicalPatterns: (state) => state.place.historicalPatterns || [],
-  loading: (state) => state.loading,
-  names: (state) => state.place.names || [],
-  place: (state) => state.place,
-  primaryName: (state) => state.place.primaryName,
-};
-
-const mutations = {
-  get(state, placeId) {
-    state.loading = true;
-    return api
-      .get(placeId)
-      .then(({ data, relationships }) => {
-        data.names = relationships.names.data;
-        data.historicalPatterns = relationships.historicalPatterns.data;
-        state.place = data;
-        return state.place;
-      })
-      .finally(() => {
-        state.loading = false;
-      });
-  },
-};
-
-const actions = {
-  initialize({ commit }, placeId) {
-    return commit('get', placeId);
-  },
-  initializeOrGetCached({ commit, state }, placeId) {
-    if (!isEmpty(state.place) && !state.loading && state.place.id == placeId) {
-      return state.place;
-    }
-
-    return commit('get', placeId);
-  },
-};
-
 export default {
   namespaced: true,
-  state,
-  getters,
-  mutations,
-  actions,
+  state: () => ({ place: {}, loading: false }),
+  getters: {
+    hasPendingChanges: (state) => state.place.hasPendingChanges,
+    historicalPatterns: (state) => state.place.historicalPatterns || [],
+    loading: (state) => state.loading,
+    names: (state) => state.place.names || [],
+    place: (state) => state.place,
+    primaryName: (state) => state.place.primaryName,
+  },
+  mutations: {
+    setLoading: (state, value) => {
+      state.loading = value;
+    },
+    setPlace: (state, value) => {
+      state.place = value;
+    },
+  },
+  actions: {
+    get({ state, commit }, placeId) {
+      commit('setLoading', true);
+      return api
+        .get(placeId)
+        .then(({ data, relationships }) => {
+          data.names = relationships.names.data;
+          data.historicalPatterns = relationships.historicalPatterns.data;
+          commit('setPlace', data);
+          return state.place;
+        })
+        .finally(() => {
+          commit('setLoading', false);
+        });
+    },
+    initialize({ dispatch }, placeId) {
+      return dispatch('get', placeId);
+    },
+    initializeOrGetCached({ dispatch, state }, placeId) {
+      if (
+        !isEmpty(state.place) &&
+        !state.loading &&
+        state.place.id == placeId
+      ) {
+        return state.place;
+      }
+      return dispatch('get', placeId);
+    },
+  },
 };
