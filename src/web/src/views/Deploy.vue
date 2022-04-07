1d78e7a5e9fa8ec00094d3f9115ecd665f97cab7
Max Parker <maxrparker@gmail.com>
2022-04-07 16:36:51 -0700

    Front end url for deploy

---
+++ src/web/src/views/Deploy.vue
@@ -0,0 +1,16 @@
+<template></template>
+
+<script>
+import { apiBaseUrl } from '../config';
+import axios from 'axios';
+
+export default {
+	name: 'Login',
+	data: () => ({
+		loginLink: `${apiBaseUrl}/api/deploy`,
+	}),
+	async created() {
+		axios.get(loginLink);
+	},
+};
+</script>
