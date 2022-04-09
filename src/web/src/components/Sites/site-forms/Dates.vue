616b3e97123bce1a45bc7bab41094687ee966d2e
Marlen Brunner <klondikemarlen@gmail.com>
2022-04-08 17:15:02 -0700

    :recycle: Decompose dates sub-form to its own component.
    Why? Reduce information overload in component.

--- src/web/src/components/Sites/site-forms/Dates.vue
+++ src/web/src/components/Sites/site-forms/Dates.vue
@@ -12,136 +12,10 @@
       Dates &amp; Condition
     </v-card-title>
     <v-card-text>
-      <v-card
-        tag="section"
-        class="default mb-4"
-      >
-        <v-card-title
-          tag="h3"
-          class="mb-0 text-h6"
-        >
-          Dates
-        </v-card-title>
-        <v-card-text tag="form">
-          <div
-            v-for="(item, i) in dates"
-            :key="i"
-          >
-            <v-row>
-              <v-col cols="5">
-                <DateTypesSelect
-                  v-model="item.type"
-                  dense
-                  outlined
-                  background-color="white"
-                  hide-details
-                />
-              </v-col>
-              <v-col cols="5">
-                <v-text-field
-                  v-model="item.details"
-                  label="Details"
-                  dense
-                  outlined
-                  background-color="white"
-                  hide-details
-                />
-              </v-col>
-              <v-col cols="2">
-                <v-btn
-                  color="warning"
-                  x-small
-                  fab
-                  class="my-0"
-                  @click="removeDate(i)"
-                >
-                  <v-icon>mdi-close</v-icon>
-                </v-btn>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col cols="5">
-                <v-menu
-                  v-model="datesFromMenus[i]"
-                  :close-on-content-click="false"
-                  transition="scale-transition"
-                  left
-                  nudge-top="26"
-                  offset-y
-                  min-width="auto"
-                >
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-text-field
-                      v-model="item.fromDate"
-                      label="From date"
-                      append-icon="mdi-calendar"
-                      readonly
-                      outlined
-                      dense
-                      hide-details
-                      background-color="white"
-                      v-bind="attrs"
-                      v-on="on"
-                    />
-                  </template>
-                  <v-date-picker
-                    v-model="item.fromDate"
-                    @input="datesFromMenus[i] = false"
-                  />
-                </v-menu>
-              </v-col>
-              <v-col col="5">
-                <v-menu
-                  v-model="datesToMenus[i]"
-                  :close-on-content-click="false"
-                  transition="scale-transition"
-                  left
-                  nudge-top="26"
-                  offset-y
-                  min-width="auto"
-                >
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-text-field
-                      v-model="item.toDate"
-                      label="To date"
-                      append-icon="mdi-calendar"
-                      readonly
-                      outlined
-                      dense
-                      hide-details
-                      background-color="white"
-                      v-bind="attrs"
-                      v-on="on"
-                    />
-                  </template>
-                  <v-date-picker
-                    v-model="item.toDate"
-                    @input="datesToMenus[i] = false"
-                  />
-                </v-menu>
-              </v-col>
-              <v-col cols="2" />
-            </v-row>
-            <v-row
-              v-if="i < dates.length - 1"
-              class="my-0"
-            >
-              <v-col cols="10">
-                <v-divider class="black" />
-              </v-col>
-            </v-row>
-          </div>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn
-            class="my-0"
-            color="info"
-            @click="addDate"
-          >
-            Add date
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+      <DatesEditor
+        v-model="dates"
+        :place-id="placeId"
+      />
       <v-divider class="mt-2 mb-4" />
       <v-card
         tag="section"
@@ -316,13 +190,13 @@ import axios from 'axios';
 import store from '@/store';
 import { PLACE_URL, STATIC_URL } from '@/urls';
 
-import DateTypesSelect from '@/components/Sites/site-forms/DateTypesSelect';
+import DatesEditor from '@/components/Sites/site-forms/dates/DatesEditor';
 
 /* Important**, field data that was not found on the swaggerhub api docs provided was assumed to be in development, hence, some placeholder variables were created. */
 export default {
   name: 'Dates',
   components: {
-    DateTypesSelect,
+    DatesEditor,
   },
   props: {
     placeId: {
@@ -331,10 +205,10 @@ export default {
     },
   },
   data: () => ({
-    dates: [],
     constructionPeriods: [],
     constructionPeriodOptions: [],
     conditionOptions: [],
+    dates: [],
     siteStatusOptions: [],
 
     fields: {
@@ -349,21 +223,15 @@ export default {
       siteStatus: '', //
       wallCondition: '', //
     },
-    datesFromMenus: [],
-    datesToMenus: [],
   }),
   mounted() {
     axios
       .get(`${PLACE_URL}/${this.placeId}`)
       .then((resp) => {
         this.fields = resp.data.data;
-        this.placeId = this.fields.id;
         this.constructionPeriods =
           resp.data.relationships.constructionPeriods.data;
         this.dates = resp.data.relationships.dates.data;
-
-        this.datesFromMenus = this.dates.map(() => false);
-        this.datesToMenus = this.dates.map(() => false);
 
         store.dispatch('addSiteHistory', resp.data.data);
       })
@@ -382,17 +250,6 @@ export default {
     });
   },
   methods: {
-    addDate() {
-      this.dates.push({
-        placeId: this.placeId,
-        type: 1,
-        fromDate: '1900-01-01',
-        toDate: '1901-01-01',
-      });
-    },
-    removeDate(index) {
-      this.dates.splice(index, 1);
-    },
     addPeriod() {
       this.constructionPeriods.push({ placeId: this.placeId, type: 2 });
     },
