8690da21af2fe9c33f6a85a60625192c000cb573
Marlen Brunner <klondikemarlen@gmail.com>
2022-04-20 08:40:43 -0700

    :heavy_plus_sign: Add testdouble and testdouble-chai for test mocking.

--- src/api/tests/test-setup.js
+++ src/api/tests/test-setup.js
@@ -1,6 +1,10 @@
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
+const td = require('testdouble');
+const testdoubleChai = require('testdouble-chai');
 
 chai.use(chaiAsPromised);
+chai.use(testdoubleChai(td));
 
 global.expect = chai.expect;
+global.td = td;
