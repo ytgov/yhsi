cea92ba37471bd86f1a8c27c96e2c5a9199bd087
Marlen Brunner <klondikemarlen@gmail.com>
2022-04-21 09:09:53 -0700

    :recycle: Support camel casing on User and UserSiteAccess records.
    Why? The standard casing in JS in camel casing.

--- src/api/models/user-site-access.ts
+++ src/api/models/user-site-access.ts
@@ -1,4 +1,6 @@
-export interface UserSiteAccess {
+import { PlainObject } from '.';
+
+export class UserSiteAccess {
 	id: number;
 	user_id: number;
 	access_type_id: number;
@@ -6,4 +8,50 @@ export interface UserSiteAccess {
 
 	access_type_name?: string;
 	access_text_name?: string;
+
+	constructor(attributes: PlainObject) {
+		this.id = attributes.id;
+		this.user_id = attributes.user_id || attributes.userId;
+		this.access_type_id = attributes.access_type_id || attributes.accessTypeId;
+		this.access_text = attributes.access_text || attributes.accessText;
+		this.access_type_name =
+			attributes.access_type_name || attributes.accessTypeName;
+		this.access_text_name =
+			attributes.access_text_name || attributes.accessTextName;
+	}
+
+	get userId(): number {
+		return this.user_id;
+	}
+	set userId(value: number) {
+		this.user_id = value;
+	}
+
+	get accessTypeId(): number {
+		return this.access_type_id;
+	}
+	set accessTypeId(value: number) {
+		this.access_type_id = value;
+	}
+
+	get accessText(): number | string {
+		return this.access_text;
+	}
+	set accessText(value: number | string) {
+		this.access_text = value;
+	}
+
+	get accessTypeName(): string {
+		return this.access_type_name;
+	}
+	set accessTypeName(value: string) {
+		this.access_type_name = value;
+	}
+
+	get accessTextName(): string {
+		return this.access_text_name;
+	}
+	set accessTextName(value: string) {
+		this.access_text_name = value;
+	}
 }
--- src/api/models/user.ts
+++ src/api/models/user.ts
@@ -18,47 +18,84 @@ export class User {
 
 	constructor(attributes: PlainObject) {
 		this.id = attributes.id;
-		this.create_date = attributes.create_date;
+		this.create_date = attributes.create_date || attributes.createDate;
 		this.email = attributes.email;
-		this.expire_date = attributes.expire_date;
-		this.expire_date_display = attributes.expire_date_display;
-		this.first_name = attributes.first_name;
-		this.last_login_date = attributes.last_login_date;
-		this.last_login_date_display = attributes.last_login_date_display;
-		this.last_name = attributes.last_name;
-		this.role_list = attributes.role_list;
+		this.expire_date = attributes.expire_date || attributes.expireDate;
+		this.expire_date_display =
+			attributes.expire_date_display || attributes.expireDateDisplay;
+		this.first_name = attributes.first_name || attributes.firstName;
+		this.last_login_date =
+			attributes.last_login_date || attributes.lastLoginDate;
+		this.last_login_date_display =
+			attributes.last_login_date_display || attributes.lastLoginDateDisplay;
+		this.last_name = attributes.last_name || attributes.lastName;
+		this.role_list = attributes.role_list || attributes.roleList;
 		this.roles = attributes.roles;
-		this.site_access = attributes.site_access;
+		this.site_access = attributes.site_access || attributes.siteAccess;
 		this.status = attributes.status;
 	}
 
 	get createDate() {
 		return this.create_date;
 	}
+	set createDate(value: Date) {
+		this.create_date = value;
+	}
+
 	get expireDate() {
 		return this.expire_date;
 	}
+	set expireDate(value: Date) {
+		this.expire_date = value;
+	}
+
 	get expireDateDisplay() {
 		return this.expire_date_display;
 	}
+	set expireDateDisplay(value: string) {
+		this.expire_date_display = value;
+	}
+
 	get firstName() {
 		return this.first_name;
 	}
+	set firstName(value: string) {
+		this.first_name = value;
+	}
+
 	get lastLoginDate() {
 		return this.last_login_date;
 	}
+	set lastLoginDate(value: Date) {
+		this.last_login_date = value;
+	}
+
 	get lastLoginDateDisplay() {
 		return this.last_login_date_display;
 	}
+	set lastLoginDateDisplay(value: string) {
+		this.last_login_date_display = value;
+	}
+
 	get lastName() {
 		return this.last_name;
+	}
+	set lastName(value: string) {
+		this.last_name = value;
 	}
 
 	get roleList(): string[] {
 		return this.roles?.split(',') || [];
 	}
+	set roleList(value: string[]) {
+		this.role_list = value;
+		this.roles = value.join(',');
+	}
 
 	get siteAccess() {
 		return this.site_access || [];
+	}
+	set siteAccess(value: UserSiteAccess[]) {
+		this.site_access = value || [];
 	}
 }
