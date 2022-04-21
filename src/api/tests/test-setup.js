ca7ee992a365ff528f7b7946739ff935c4130eac
Marlen Brunner <klondikemarlen@gmail.com>
2022-04-21 08:18:02 -0700

    :heavy_plus_sign: Add faker for generating randomness in tests.
    Note that the compiler options `"esModuleInterop": true` and `"moduleResolution": "Node"` are required by faker.

--- src/api/tests/test-setup.js
+++ src/api/tests/test-setup.js
@@ -2,9 +2,11 @@ const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 const td = require('testdouble');
 const testdoubleChai = require('testdouble-chai');
+const { faker } = require('@faker-js/faker');
 
 chai.use(chaiAsPromised);
 chai.use(testdoubleChai(td));
 
 global.expect = chai.expect;
 global.td = td;
+global.faker = faker;
