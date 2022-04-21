51c574734b21eaef556480820cda0a3bae2fe43b
Marlen Brunner <klondikemarlen@gmail.com>
2022-04-21 09:23:40 -0700

    :white_check_mark: Add test for policy lock on map sheet site access.

--- src/api/tests/policies/place-policy.test.ts
+++ src/api/tests/policies/place-policy.test.ts
@@ -1,4 +1,4 @@
-import { User, UserRoles } from '../../models';
+import { User, UserRoles, UserSiteAccess, SiteAccesType } from '../../models';
 import { PlacePolicy } from '../../policies';
 
 describe('PlacePolicy', () => {
@@ -18,6 +18,55 @@ describe('PlacePolicy', () => {
 
 				it('permits access', () => {
 					expect($policy.show()).to.be.true;
+				});
+			});
+
+			context('when user has a site access role', () => {
+				def('roles', () =>
+					faker.random
+						.arrayElements([
+							UserRoles.SITE_ADMIN,
+							UserRoles.SITE_EDITOR,
+							UserRoles.SITE_VIEWER,
+						])
+						.join(',')
+				);
+
+				context('when user has no specific access', () => {
+					def('user', () => new User({ firstName: 'marlen', roles: $roles }));
+					def('place', () => ({ id: 1, yHSIId: '116B/03/600' }));
+
+					it('does not permits access', () => {
+						expect($policy.show()).to.be.false;
+					});
+				});
+
+				context('when user has specific access via the NTS Map Sheet', () => {
+					def('nTSMapSheet', () => '115O/03');
+					def('siteAccess', () => [
+						new UserSiteAccess({
+							accessTypeId: SiteAccesType.MAP_SHEET,
+							accessText: $nTSMapSheet,
+						}),
+					]);
+					def(
+						'user',
+						() =>
+							new User({
+								firstName: 'marlen',
+								roles: $roles,
+								siteAccess: $siteAccess,
+							})
+					);
+					def('place', () => ({
+						id: 1,
+						yHSIId: '116B/03/600',
+						nTSMapSheet: $nTSMapSheet,
+					}));
+
+					it('permits access', () => {
+						expect($policy.show()).to.be.true;
+					});
 				});
 			});
 
