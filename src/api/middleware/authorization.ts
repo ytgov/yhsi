b0829313a36fc32bfe39d5029cd30442a2d0204b
Ryan Agar <ryanjagar@hey.com>
2023-10-15 16:56:20 -0400

    linting and formating

--- src/api/middleware/authorization.ts
+++ src/api/middleware/authorization.ts
@@ -4,42 +4,43 @@ import { User, UserRoles } from '../models';
 const USER_ACTIVE_STATUS = 'Active';
 
 export const UserRoleOptions = [
-    UserRoles.ADMINISTRATOR,
-    UserRoles.SITE_ADMIN,
-    UserRoles.SITE_EDITOR,
-    UserRoles.SITE_VIEWER,
-    UserRoles.SITE_VIEWER_LIMITED,
-    UserRoles.PHOTO_ADMIN,
-    UserRoles.PHOTO_EDITOR,
-    UserRoles.PHOTO_VIEWER,
-    UserRoles.AIRPLANE_CRASH_EDITOR,
-    UserRoles.BOATS_EDITOR,
-    UserRoles.PLACE_EDITOR,
-    UserRoles.BURIALS_EDITOR,
-    UserRoles.PEOPLE_EDITOR,
-    UserRoles.YRHP_EDITOR,
-    UserRoles.ADVANCED_MAPS
-]
+	UserRoles.ADMINISTRATOR,
+	UserRoles.SITE_ADMIN,
+	UserRoles.SITE_EDITOR,
+	UserRoles.SITE_VIEWER,
+	UserRoles.SITE_VIEWER_LIMITED,
+	UserRoles.PHOTO_ADMIN,
+	UserRoles.PHOTO_EDITOR,
+	UserRoles.PHOTO_VIEWER,
+	UserRoles.AIRPLANE_CRASH_EDITOR,
+	UserRoles.BOATS_EDITOR,
+	UserRoles.PLACE_EDITOR,
+	UserRoles.BURIALS_EDITOR,
+	UserRoles.PEOPLE_EDITOR,
+	UserRoles.YRHP_EDITOR,
+	UserRoles.ADVANCED_MAPS,
+];
 
-export function authorize(roles: string[] = [], allowPending: boolean = false) {
-    return (req: Request, res: Response, next: NextFunction) => {
-        let currentUser = req.user as User;
+export function authorize(roles: string[] = [], allowPending = false) {
+	return (req: Request, res: Response, next: NextFunction) => {
+		const currentUser = req.user as User;
 
-        if (!req.oidc.isAuthenticated() || !currentUser)
-            return res.status(401).send('Not authenticated');
+		if (!req.oidc.isAuthenticated() || !currentUser)
+			return res.status(401).send('Not authenticated');
 
-        if (currentUser.status != USER_ACTIVE_STATUS && !allowPending)
-            return res.status(401).json({ message: "Unauthorized - User inactive" });
+		if (currentUser.status != USER_ACTIVE_STATUS && !allowPending)
+			return res.status(401).json({ message: 'Unauthorized - User inactive' });
 
-        // if route only requires an active user
-        if (roles.length == 0)
-            return next();
+		// if route only requires an active user
+		if (roles.length == 0) return next();
 
-        for (let role of roles) {
-            if (currentUser.roles && currentUser.roles.indexOf(role) >= 0)
-                return next();
-        }
+		for (const role of roles) {
+			if (currentUser.roles && currentUser.roles.indexOf(role) >= 0)
+				return next();
+		}
 
-        return res.status(401).json({ message: 'Unauthorized - Missing role(s): ' + roles });
-    };
+		return res
+			.status(401)
+			.json({ message: 'Unauthorized - Missing role(s): ' + roles });
+	};
 }
