3d255a289b6db12ecbbb6e673d89b31a5a441973
Ryan Agar <ryanjagar@hey.com>
2023-12-01 17:31:21 -0700

    change routes to pass params by function

--- src/web/src/modules/AirplaneCrashes/router/AircrashRouter.js
+++ src/web/src/modules/AirplaneCrashes/router/AircrashRouter.js
@@ -7,20 +7,28 @@ const routes = [
 		meta: { requiresAuth: true, authorize: [UserRoles.AIRPLANE_CRASH_EDITOR] },
 	},
 	{
-		path: 'view/:name',
+		path: 'view/:crashID',
 		name: 'airplaneView',
 		component: () => import('../views/AirplaneCrashForm'),
-		props: { action: 'view' },
+		props: (route) => {
+			return { action: 'view', ...route.params };
+		},
 		meta: {
 			requiresAuth: true,
 			authorize: [UserRoles.AIRPLANE_CRASH_EDITOR],
 		},
 	},
 	{
-		path: 'edit/:name',
+		path: 'edit/:crashID',
 		name: 'airplaneEditView',
 		component: () => import('../views/AirplaneCrashForm'),
-		props: { action: 'edit' },
+		//Setting props as a function gives use the best of both worlds
+		//we can use route params to get the crash ID
+		//and we can set an action.  This allows us to set different security
+		//different actions at the route level
+		props: (route) => {
+			return { action: 'edit', ...route.params };
+		},
 		meta: {
 			requiresAuth: true,
 			authorize: [UserRoles.AIRPLANE_CRASH_EDITOR],
