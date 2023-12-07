1fb48e86799d387f308b8a5cb6ad22907b6cdb32
Ryan Agar <ryanjagar@hey.com>
2023-12-07 16:24:53 -0700

    inforsouce component

--- src/web/src/modules/AirplaneCrashes/components/InfoSources.vue
+++ src/web/src/modules/AirplaneCrashes/components/InfoSources.vue
@@ -1,6 +1,5 @@
 <template>
 	<div>
-		{{ deletedSources }}
 		<!-- Information source list -->
 		<v-card>
 			<v-list class="pa-0">
@@ -9,10 +8,10 @@
 				<template v-for="(item, index) in infoSources">
 					<v-list-item :key="`nl-${index}`">
 						<v-list-item-content>
-							<v-list-item-title
-								v-if="index != editTableSources || isViewingCrash"
-								>{{ item.Source }}</v-list-item-title
-							>
+							<v-list-item-title v-if="!isBeingEdited(index)">{{
+								item.Source
+							}}</v-list-item-title>
+							<!-- Start of Editing -->
 							<v-form
 								v-model="validSource"
 								v-if="!isViewingCrash"
@@ -21,85 +20,89 @@
 								<v-text-field
 									outlined
 									dense
-									v-if="editTableSources == index"
+									v-if="isBeingEdited(index)"
 									label="Source"
-									v-model="helperSource"
+									v-model="sourceText"
 									:rules="sourceRules"
 								></v-text-field>
 							</v-form>
+
+							<!-- End of Editing -->
 						</v-list-item-content>
+						<!-- Buttons -->
 						<v-list-item-action class="d-flex flex-row">
-							<v-tooltip
-								bottom
-								v-if="!isViewingCrash && editTableSources != index"
-							>
-								<template v-slot:activator="{ on, attrs }">
-									<v-btn
-										v-bind="attrs"
-										v-on="on"
-										icon
-										class="grey--text text--darken-2"
-										@click="deleteSource(item, index)"
-									>
-										<v-icon small> mdi-delete</v-icon>
-									</v-btn>
-								</template>
-								<span>Delete</span>
-							</v-tooltip>
-							<v-tooltip
-								bottom
-								v-if="!isViewingCrash && editTableSources != index"
-							>
-								<template v-slot:activator="{ on, attrs }">
-									<v-btn
-										v-bind="attrs"
-										v-on="on"
-										icon
-										class="grey--text text--darken-2"
-										@click="changeEditTableSources(item, index)"
-									>
-										<v-icon small> mdi-pencil</v-icon>
-									</v-btn>
-								</template>
-								<span>Edit</span>
-							</v-tooltip>
-							<v-tooltip
-								bottom
-								v-if="!isViewingCrash && editTableSources == index"
-							>
-								<template v-slot:activator="{ on, attrs }">
-									<v-btn
-										v-bind="attrs"
-										v-on="on"
-										:disabled="!validSource"
-										icon
-										class="grey--text text--darken-2"
-										color="success"
-										@click="saveTableSources(index)"
-									>
-										<v-icon small>mdi-check</v-icon>
-									</v-btn>
-								</template>
-								<span>Save changes</span>
-							</v-tooltip>
-							<v-tooltip
-								bottom
-								v-if="!isViewingCrash && editTableSources == index"
-							>
-								<template v-slot:activator="{ on, attrs }">
-									<v-btn
-										v-bind="attrs"
-										v-on="on"
-										icon
-										class="grey--text text--darken-2"
-										@click="cancelEditTableSources()"
-									>
-										<v-icon small>mdi-close</v-icon>
-									</v-btn>
-								</template>
-								<span>Cancel</span>
-							</v-tooltip>
+							<div v-if="canAddOrRemoveSource">
+								<v-tooltip bottom>
+									<template v-slot:activator="{ on, attrs }">
+										<v-btn
+											v-bind="attrs"
+											v-on="on"
+											icon
+											class="grey--text text--darken-2"
+											@click="doDeleteSource(item, index)"
+										>
+											<v-icon small> mdi-delete</v-icon>
+										</v-btn>
+									</template>
+									<span>Delete</span>
+								</v-tooltip>
+								<v-tooltip bottom>
+									<template v-slot:activator="{ on, attrs }">
+										<v-btn
+											v-bind="attrs"
+											v-on="on"
+											icon
+											class="grey--text text--darken-2"
+											@click="doEditSource(item, index)"
+										>
+											<v-icon small> mdi-pencil</v-icon>
+										</v-btn>
+									</template>
+									<span>Edit</span>
+								</v-tooltip>
+							</div>
+							<!-- Buttons for confirming edis -->
+							<div v-if="isBeingEdited(index)">
+								<v-tooltip bottom>
+									<template v-slot:activator="{ on, attrs }">
+										<v-btn
+											v-bind="attrs"
+											v-on="on"
+											:disabled="!validSource"
+											icon
+											class="grey--text text--darken-2"
+											color="success"
+											@click="saveSource(item, index)"
+										>
+											<v-icon small>mdi-check</v-icon>
+										</v-btn>
+									</template>
+									<span>Save changes</span>
+								</v-tooltip>
+								<v-tooltip bottom>
+									<template v-slot:activator="{ on, attrs }">
+										<v-btn
+											v-bind="attrs"
+											v-on="on"
+											icon
+											class="grey--text text--darken-2"
+											@click="cancelEdit(item, index)"
+										>
+											<!-- <v-btn
+											v-bind="attrs"
+											v-on="on"
+											icon
+											class="grey--text text--darken-2"
+											@click="cancelEditTableSources()"
+										> -->
+											<v-icon small>mdi-close</v-icon>
+										</v-btn>
+									</template>
+									<span>Cancel</span>
+								</v-tooltip>
+							</div>
 						</v-list-item-action>
+						<!-- Buttons -->
 					</v-list-item>
 					<v-divider :key="`ldiv-${index}`"></v-divider>
 				</template>
@@ -113,19 +116,17 @@
 				<v-spacer></v-spacer>
 				<v-btn
 					class="mx-1 black--text align"
-					@click="addSource"
-					v-if="!isViewingCrash && editTableSources == -1"
+					@click="addNewSource"
+					v-if="canAddOrRemoveSource"
 					>Add Source</v-btn
 				>
 			</v-col>
 		</v-row>
-		{{ editedInfoSources }}
-		{{ removedInfoSources }}
 	</div>
 </template>
 
 <script>
-import { mapActions, mapGetters, mapMutations } from 'vuex';
+import { mapActions, mapGetters, mapMutations, mapState } from 'vuex';
 // import _ from 'lodash';
 export default {
 	name: 'InfoSources',
@@ -134,28 +135,33 @@ export default {
 			type: String,
 			default: 'view',
 		},
-		// infoSources: {
-		// 	type: Array,
-		// 	// default: () => [''],
-		// },
 	},
 	data: () => ({
-		editTableSources: -1, // tells the list which element will be edited (it has problems with accuracy, i.e: you cant distinguish between an edit & a new element being added)
-		addingSource: false, // tells the list if the user is adding a new element, this helps distinguish between an edit & a new element being added...
-		helperSource: null,
 		validSource: false,
 		deletedSources: [],
 		sourceRules: [(v) => !!v || 'Source is required'],
 		newSource: {},
+		sourceText: '',
 	}),
 
 	computed: {
+		...mapState('aircrash', ['indexOfSourceBeingEdited']),
+
 		...mapGetters('aircrash', [
 			'infoSources',
 			'editedInfoSources',
 			'removedInfoSources',
 		]),
 
+		canAddOrRemoveSource() {
+			if (this.action == 'view') {
+				return false;
+			}
+			if (this.indexOfSourceBeingEdited !== null) {
+				return false;
+			}
+			return true;
+		},
 		isNewCrash() {
 			return this.action == 'new';
 		},
@@ -169,66 +175,49 @@ export default {
 
 	methods: {
 		...mapActions('aircrash', [
-			'updateInfoSources',
-			'updateDeletedSources',
 			'newInfoSource',
+			'addNewSource',
+			'deleteSource',
 		]),
-		...mapMutations('aircrash', ['deleteInfoSource', 'upsertInfoSource']),
-		changeEditTableSources(item, index) {
-			console.log(index);
-			// this.editTableSources = index;
-			this.helperSource = item.Source;
+		...mapMutations('aircrash', [
+			'deleteInfoSource',
+			'upsertInfoSource',
+			'setEdit',
+		]),
+
+		isBeingEdited: function (index) {
+			if (this.isViewingCrash) {
+				return false;
+			}
+			return this.indexOfSourceBeingEdited === index;
+		},
+		doEditSource(item, index) {
+			this.sourceText = item.Source;
+			this.setEdit(index);
 		},
 
-		deleteSource(item, index) {
-			console.log(item, index);
-			this.deleteInfoSource({ item, index });
-			// if (index > -1) {
-			// 	this.infoSources.splice(index, 1);
+		cancelEdit(item, index) {
+			if (item.status == 'New') {
+				this.deleteInfoSource({ item, index });
+			}
+			this.setEdit(null);
+		},
 
-			// 	if (!item.isNew) {
-			// 		this.updateDeletedSources(item);
-			// 		this.deletedSources.push(item);
-			// 		this.updateInfoSources(this.infoSources);
-			// 	}
-			// }
-		},
-		cancelEditTableSources() {
-			if (this.addingSource) {
-				this.infoSources.pop();
-				this.addingSource = false;
-				// this.editTableSources = -1;
-			} else {
-				this.editTableNames = -1;
+		saveSource(item, index) {
+			if (item.Source !== this.sourceText) {
+				item.Source = this.sourceText;
+				this.upsertInfoSource({ item, index });
 			}
+			this.sourceText = '';
+			this.setEdit(null);
 		},
-		saveTableSources(index) {
-			if (this.addingSource)
-				this.infoSources[index] = {
-					Source: this.helperSource,
-					Type: 'Reference',
-					isNew: true,
-				};
-			else {
-				this.infoSources[index].Source = this.helperSource;
-				if (!this.infoSources[index].isNew) {
-					this.infoSources[index].isEdited = true;
-				}
-			}
-			this.updateInfoSources(this.infoSources);
-			this.addingSource = false;
-			// this.editTableSources = -1;
-		},
+
 		addSource() {
 			this.newSource = this.newInfoSource();
-			this.helperSource = '';
-			// this.infoSources.push();
-			this.addingSource = true;
-			// this.editTableSources = this.infoSources.length - 1;
 		},
-		// getSources() {
-		// return _.join(this.infoSources, ';');
-		// },
+		doDeleteSource(item, index) {
+			this.deleteInfoSource({ item, index });
+		},
 	},
 };
 </script>
