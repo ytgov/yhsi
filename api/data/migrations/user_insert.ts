848fc3681b5a1e711a47dfac8e8562b1907bb4d5
caleb <calebburke91@gmail.com>
2025-10-22 17:19:08 -0700

    removed dangerous migration

--- api/data/migrations/user_insert.ts
+++ api/data/migrations/user_insert.ts
@@ -1,11 +1,4 @@
 import { Knex } from 'knex';
-exports.up = async function (knex: Knex, Promise: any) {
-	await knex.raw(`INSERT INTO Security.[User] (email, first_name, last_name, create_date, roles, status) VALUES
-	('ryanjagar@gmail.com', 'Ryan', 'Agar', '2023-05-15 14:58:46.000', 'Administrator', 'Active'),
-	('diedre@driftgeomatics.com', 'Diedre', 'Davidson', '2023-05-16 01:28:18.000', 'Administrator', 'Active'),
-	('maxrparker@gmail.com', 'Max', 'Parker', '2023-06-06 17:53:18.000', 'Administrator', 'Active')`);
-};
+exports.up = async function (knex: Knex, Promise: any) {};
 
-exports.down = async function (knex: Knex, Promise: any) {
-	await knex.raw(`DELETE FROM Security.[User] WHERE 1=1`);
-};
+exports.down = async function (knex: Knex, Promise: any) {};
